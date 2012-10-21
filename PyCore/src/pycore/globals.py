1b00461409d43b64ed5b197eb25012ea752d755f
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-10-21 11:49:00 +0000

    add a program parameter named program_dir to point out starting place for all configuration/plugins file or dictionaries used by the application
    git-svn-id: https://jurek-pc3/repositories/doctorate@57 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyCore/src/pycore/globals.py
+++ PyCore/src/pycore/globals.py
@@ -11,10 +11,12 @@ parser.set_default("numpy_usage", "true")
 
 parser.add_option("-d", "--dir_data", type="string")
 parser.add_option("-e", "--ext_mask", type="string")
+parser.add_option("-p", "--program_dir", type="string")
 
 opts, args = parser.parse_args()
 
 
 DIR_DATA = opts.dir_data
 EXT_MASK = opts.ext_mask
+PROGRAM_DIR = opts.program_dir
 NUMPY_USAGE = (opts.numpy_usage == "true")
