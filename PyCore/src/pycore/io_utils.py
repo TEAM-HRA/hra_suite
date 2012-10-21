8fa14cc350fd3c60358da1847cf6befa6078a0a7
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-10-21 11:51:23 +0000

    add a file with some useful util file/dictionary management stuff
    git-svn-id: https://jurek-pc3/repositories/doctorate@59 13b80749-e00b-5047-ae1c-0d8bb577e5e7

---
+++ PyCore/src/pycore/io_utils.py
@@ -0,0 +1,44 @@
+'''
+Created on 20-10-2012
+
+@author: jurek
+'''
+
+import sys
+from os.path import join
+from os.path import exists
+from os import walk
+from pycore.globals import PROGRAM_DIR
+
+
+def get_program_path():
+    return PROGRAM_DIR if PROGRAM_DIR else sys.path[0]
+
+
+def get_filenames(path, depth=1):
+    filenames = []
+    if (exists(path)):
+        current_depth = 1
+        for root, dirs, files in walk(path):
+            filenames[len(files):] = files
+            if (current_depth == depth):
+                break
+            current_depth += 1
+    else:
+        return filenames
+
+
+def expand_files(path, extension=None):
+    full_filenames = []
+    if extension:
+        if extension.startswith("*."):
+            extension = extension[1:]
+        elif not extension.startswith("."):
+            extension = "." + extension
+
+    if (exists(path)):
+        for paths, dirnames, files in walk(path):
+            full_filenames[len(full_filenames):] = [join(path, file)
+                for file in files if (extension == None or file.endswith(extension))] #@IgnorePep8
+
+    return full_filenames
