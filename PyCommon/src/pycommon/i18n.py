e890ab5fa8495f4db9197af2b474531c5e70546c
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-11-05 00:03:47 +0000

    add new module pycommon/i18n.py with functionality concerning internationalization; add a new package pymed/qt/wizards as a coomon place for qt wizards add a new module datasource_wizard with initial code to construct wizard for imports of datasources
    git-svn-id: https://jurek-pc3/repositories/doctorate@87 13b80749-e00b-5047-ae1c-0d8bb577e5e7

---
+++ PyCommon/src/pycommon/i18n.py
@@ -0,0 +1,17 @@
+'''
+Created on 04-11-2012
+
+@author: jurek
+'''
+from pycore.globals import GLOBALS
+from pycore.properties import Properties
+from pycore.collections import replace_all_by_dict
+
+__I18N_PROPERTIES = Properties(GLOBALS.get(I18N_FILE=GLOBALS.ITEM,
+                                           PARAM1=GLOBALS.LANG),
+                                    _file_prefix=GLOBALS.SETTINGS_DIR)
+
+
+def I18N(_id, _default=None, **params):
+    i18n = __I18N_PROPERTIES.getValue(_id)
+    return replace_all_by_dict(i18n, params) if i18n else _default
---
+++ PyMed/src/pymed/qt/wizards/datasource_wizards.py
@@ -0,0 +1,55 @@
+'''
+Created on 03-11-2012
+
+@author: jurek
+'''
+from PyQt4.QtCore import *  # @UnusedWildImport
+from PyQt4.QtGui import *  # @UnusedWildImport
+from pymed.qt.utils.context import Context
+from pycommon.i18n import I18N
+
+
+class DatasourceWizard(QWizard):
+
+    @staticmethod
+    def show_wizard():
+        parent = Context(DatasourceWizard.show_wizard).load().parent
+        return DatasourceWizard(parent).show()
+
+    def __init__(self, _parent):
+        QWizard.__init__(self, _parent)
+        self.setWindowTitle(QString(I18N("datasource.import.title",
+                                         _default="Datasource import")))
+
+    def show(self):
+        self.addPage(ChooseDatasourcePage(self))
+        self.addPage(ChooseColumnsDataPage(self))
+        self.exec_()
+
+
+class ChooseDatasourcePage(QWizardPage):
+
+    def __init__(self, _parent):
+        QWizardPage.__init__(self, parent=_parent)
+
+    def initializePage(self):
+        self.setTitle('Frames')
+        self.setSubTitle('Setup frame specific data')
+        chooseFileLabel = QLabel("Choose file name:")
+        layout = QGridLayout()
+        layout.addWidget(chooseFileLabel, 0, 0)
+        self.setLayout(layout)
+
+
+class ChooseColumnsDataPage(QWizardPage):
+
+    def __init__(self, _parent):
+        QWizardPage.__init__(self, parent=_parent)
+
+    def initializePage(self):
+        self.setTitle('Choose column data')
+        self.setSubTitle('Choose column specific data')
+        chooseFileLabel = QLabel("Specific data")
+        layout = QGridLayout()
+        layout.addWidget(chooseFileLabel, 0, 0)
+        self.setLayout(layout)
