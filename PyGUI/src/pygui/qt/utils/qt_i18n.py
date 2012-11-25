4228c1b8035c2ec4591a068a1bf69ad985f72822
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-11-25 18:52:12 +0000

    add methods to set up i18n a text or a title of an QT object
    git-svn-id: https://jurek-pc3/repositories/doctorate@136 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/utils/qt_i18n.py
+++ PyGUI/src/pygui/qt/utils/qt_i18n.py
@@ -9,3 +9,11 @@ from pycommon.i18n import I18N
 
 def QT_I18N(_id, _default=None, **params):
     return QString(I18N(_id, _default, **params))
+
+
+def title_I18N(target, _id, _default):
+    target.setTitle(QT_I18N(_id, _default=_default))
+
+
+def text_I18N(target, _id, _default):
+    target.setText(QT_I18N(_id, _default=_default))
