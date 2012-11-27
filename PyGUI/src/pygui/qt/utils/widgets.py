1d304da22835ccedb168572364014df6bfa6499f
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-11-27 21:56:18 +0000

    add a new module pygui.qt.utils.widgets with functionality to create several widgets (including some of composite type) to create nesting layout managers based on fundamental layouts: QHBoxLayout, QVBoxLayout
    git-svn-id: https://jurek-pc3/repositories/doctorate@139 13b80749-e00b-5047-ae1c-0d8bb577e5e7

---
+++ PyGUI/src/pygui/qt/utils/widgets.py
@@ -0,0 +1,121 @@
+'''
+Created on 26-11-2012
+
+@author: jurek
+'''
+from PyQt4.QtGui import *  # @UnusedWildImport
+from PyQt4.QtCore import *  # @UnusedWildImport
+from pygui.qt.utils.qt_i18n import text_I18N
+from pygui.qt.utils.qt_i18n import title_I18N
+
+
+def createComposite(parent=None, **params):
+    return __item(parent, widget=QWidget(parent), **params)
+
+
+def createLabel(parent=None, **params):
+    if params.get('sizePolicy', None) == None:
+        params['sizePolicy'] = QSizePolicy(QSizePolicy.Fixed,
+                                           QSizePolicy.Preferred)
+    return __item(parent, widget=QLabel(parent), textable=True, **params)
+
+
+def createPushButton(parent=None, **params):
+    if params.get('sizePolicy', None) == None:
+        params['sizePolicy'] = QSizePolicy(QSizePolicy.Fixed,
+                                           QSizePolicy.Fixed)
+    return __item(parent, widget=QPushButton(parent), textable=True, **params)
+
+
+def createGroupBox(parent=None, **params):
+    return __item(parent, widget=QGroupBox(parent), titleable=True, **params)
+
+
+def createLineEdit(parent=None, **params):
+    return __item(parent, widget=QLineEdit(parent), **params)
+
+
+def createCheckBox(parent=None, **params):
+    return __item(parent, widget=QCheckBox(parent), textable=True, **params)
+
+
+def createTableView(parent=None, **params):
+    return __item(parent, widget=QTableView(parent), **params)
+
+
+def createSlider(parent=None, **params):
+    return __item(parent, widget=QSlider(parent), **params)
+
+
+def createTextEdit(parent=None, **params):
+    return __item(parent, widget=QTextEdit(parent), **params)
+
+
+def __item(parent=None, **params):
+    """
+    method to create a widget based o information contained in params
+    dictionary, it is a factory method
+    """
+    parent_layout = parent.layout() \
+                if not parent == None and not parent.layout() == 0 else None
+    params = Params(**params)
+    widget = params.widget
+    if not params.sizePolicy == None:
+        widget.setSizePolicy(params.sizePolicy)
+    if not params.layout == None:
+        widget.setLayout(params.layout)
+    if not params.orientation == None:
+        widget.setOrientation(params.orientation)
+
+    added = None
+    if not params.alignment == None:
+        added = __create_inner_alignment_layout(parent_layout, widget,
+                                                params.alignment)
+        if not added and not params.layout == None:
+            params.layout.setAlignment(params.alignment)
+
+    if not added and not parent_layout == None:
+        parent_layout.addWidget(widget)
+
+    if params.titleable and (params.i18n or params.i18n_def):
+        title_I18N(widget, params.i18n, params.i18n_def)
+    if params.textable and (params.i18n or params.i18n_def):
+        text_I18N(widget, params.i18n, params.i18n_def)
+    if not params.text == None:
+        widget.setText(params.text)
+    return widget
+
+
+def __create_inner_alignment_layout(parent_layout, widget, alignment):
+    """
+    method which creates, if necessary, inner layout suitable
+    to employ effectively alignment parameter
+    """
+    inner_layout = None
+    if alignment in (Qt.AlignLeft, Qt.AlignCenter, Qt.AlignRight) \
+        and not isinstance(parent_layout, QHBoxLayout):
+        inner_layout = QHBoxLayout()
+    elif alignment in (Qt.AlignTop, Qt.AlignVCenter, Qt.AlignBottom) \
+        and not isinstance(parent_layout, QVBoxLayout):
+        inner_layout = QVBoxLayout()
+    if not inner_layout == None:
+        inner_layout.setAlignment(alignment)
+        inner_layout.addWidget(widget)
+        if not parent_layout == None:
+            parent_layout.addLayout(inner_layout)
+    return not inner_layout == None
+
+
+class Params(object):
+    """
+    class which represents dictionary parameters
+    by object where elements are accessible with dot notation
+    if client tries to access not existing element then None value is returned
+    """
+    def __init__(self, **params):
+        for param in params:
+            setattr(self, param, params[param])
+
+    # if parameter is not set in the __init__() method then returns None
+    def __getattr__(self, name):
+        return None
