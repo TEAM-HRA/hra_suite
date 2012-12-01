dd9790f9b4ee0711401de0a0ced03bd91bd4eb5b
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-12-01 19:27:31 +0000

    remove a section for building datasource group box; remove button Reload because all changes in the file name filter or search recursively check box are immediately map into files table view widget; add implementation for preview file action, a selected file is displayed in a separate window
    git-svn-id: https://jurek-pc3/repositories/doctorate@153 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyMed/src/pymed/qt/wizards/datasource_wizards.py
+++ PyMed/src/pymed/qt/wizards/datasource_wizards.py
@@ -3,11 +3,13 @@ Created on 03-11-2012
 
 @author: jurek
 '''
+from os.path import join
 from PyQt4.QtCore import *  # @UnusedWildImport
 from PyQt4.QtGui import *  # @UnusedWildImport
 from pygui.qt.utils.qt_i18n import QT_I18N
 from pygui.qt.utils.qt_i18n import title_I18N
 from pygui.qt.utils.widgets import createComposite
+from pygui.qt.utils.widgets import createPlainTextEdit
 from pygui.qt.utils.widgets import createTableView
 from pygui.qt.utils.widgets import createLineEdit
 from pygui.qt.utils.widgets import createCheckBox
@@ -15,6 +17,7 @@ from pygui.qt.utils.widgets import createLabel
 from pygui.qt.utils.widgets import createPushButton
 from pygui.qt.utils.widgets import createGroupBox
 from pygui.qt.utils.graphics import get_width_of_n_letters
+from pycore.io_utils import is_text_file
 
 
 class DatasourceWizard(QWizard):
@@ -48,61 +51,29 @@ class ChooseDatasourcePage(QWizardPage):
                   QT_I18N("datasource.files.column.path", "File path")]
         labels = QStringList(labels)
         self.model.setHorizontalHeaderLabels(labels)
+        self.selectedRow = None
+        self.rootDir = None
 
     def initializePage(self):
         title_I18N(self, "datasource.page.title", "Datasource chooser")
         #self.setSubTitle('Setup frame specific data')
         pageLayout = QVBoxLayout()
         self.setLayout(pageLayout)
-        self.__createDatasourceGroupBox(pageLayout)
         self.__createFilesGroupBox(pageLayout)
-
-    def __createDatasourceGroupBox(self, pageLayout):
-        self.datasourceGroupBox = createGroupBox(self,
-                                    i18n="datasource.datasource.group.title",
-                                    i18n_def="Datasource",
-                                    layout=QHBoxLayout())
-
-        self.chooseRootDirButton = createPushButton(self.datasourceGroupBox,
-                        i18n="datasource.datasource.choose.root.dir.button",
-                        i18n_def="Choose root dir",
-                        stretch_after_widget=1)
-        self.connect(self.chooseRootDirButton, SIGNAL("clicked()"),
-                     self.chooseRootDirAction)
-
-#        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-#        self.rootDirLabel = createLabel(self.datasourceGroupBox,
-#                                        i18n="datasource.root.dir.label",
-#                                        i18n_def="[Not set]",
-#                                        sizePolicy=sizePolicy)
-
-        createLabel(self.datasourceGroupBox,
-                    i18n="datasource.file.name.filter.label",
-                    i18n_def="Files name filter")
-
-        self.filesExtension = createLineEdit(self.datasourceGroupBox,
-                                             #sizePolicy=sizePolicy,
-                                             maxLength=9,
-                                             width=get_width_of_n_letters(8),
-                                             text="*")
-        self.recursively = createCheckBox(self.datasourceGroupBox,
-                i18n="datasource.search.files.recursively.label",
-                i18n_def="Search files recursively")
 
     def __createFilesGroupBox(self, pageLayout):
         self.filesGroupBox = createGroupBox(self,
                                     i18n="datasource.files.group.title",
                                     i18n_def="Files",
-                                    layout=QVBoxLayout(),
-                                    enabled=False)
+                                    layout=QVBoxLayout())
 
         _dir = createComposite(self.filesGroupBox, layout=QHBoxLayout())
 
-        self.reloadButton = createPushButton(_dir,
-                        i18n="datasource.datasource.reload.button",
-                        i18n_def="Reload")
-        self.connect(self.reloadButton, SIGNAL("clicked()"),
-                     self.reloadAction)
+        self.chooseRootDirButton = createPushButton(_dir,
+                        i18n="datasource.datasource.choose.root.dir.button",
+                        i18n_def="Choose root dir")
+        self.connect(self.chooseRootDirButton, SIGNAL("clicked()"),
+                     self.chooseRootDirAction)
 
         createLabel(_dir,
                      i18n="datasource.root.dir.label",
@@ -111,50 +82,67 @@ class ChooseDatasourcePage(QWizardPage):
         self.rootDirLabel = createLabel(_dir,
                                         i18n="datasource.root.dir.label",
                                         i18n_def="[Not set]",
-                                        sizePolicy=sizePolicy)
+                                        sizePolicy=sizePolicy,
+                                        stretch_after_widget=1)
+
+        createLabel(_dir,
+                    i18n="datasource.file.name.filter.label",
+                    i18n_def="Files name filter")
+
+        self.filesExtension = createLineEdit(_dir,
+                                             maxLength=9,
+                                             width=get_width_of_n_letters(8),
+                                             text="*")
+        self.connect(self.filesExtension,
+                     SIGNAL("textChanged(const QString&)"),
+                     self.filesExtensionAction)
+
+        self.recursively = createCheckBox(_dir,
+                i18n="datasource.search.files.recursively.label",
+                i18n_def="Search files recursively")
+        self.connect(self.recursively, SIGNAL("clicked()"),
+                     self.recursivelyAction)
 
         self.filesTableView = createTableView(self.filesGroupBox,
                         selectionBehavior=QAbstractItemView.SelectRows,
                         selectionMode=QAbstractItemView.SingleSelection)
         self.filesTableView.setModel(self.model)
         self.filesTableView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.connect(self.filesTableView, SIGNAL('clicked(QModelIndex)'),
+                     self.onClickedAction)
 
         filesOperations = createComposite(self.filesGroupBox,
                                             layout=QHBoxLayout())
 
-        filePreviewButton = createPushButton(filesOperations,
+        self.filePreviewButton = createPushButton(filesOperations,
                             i18n="datasource.file.preview.button",
                             i18n_def="File preview",
                             stretch_after_widget=1)
-        self.connect(filePreviewButton, SIGNAL("clicked()"),
+        self.connect(self.filePreviewButton, SIGNAL("clicked()"),
                      self.filePreviewAction)
 
-        createLabel(filesOperations,
-                     i18n="datasource.re.label",
-                     i18n_def="Pattern (a regular expression)")
-
-        self.reEdit = createLineEdit(filesOperations)
-        self.checkReButton = createPushButton(filesOperations,
-                            i18n="datasource.check.regular.expression.button",
-                            i18n_def="Check reg.expr.")
-
-        checkAllButton = createPushButton(filesOperations,
+        self.checkAllButton = createPushButton(filesOperations,
                             i18n="datasource.accept.check.all.button",
                             i18n_def="Check all")
-        self.connect(checkAllButton, SIGNAL("clicked()"), self.checkAllAction)
+        self.connect(self.checkAllButton, SIGNAL("clicked()"),
+                     self.checkAllAction)
 
-        uncheckAllButton = createPushButton(filesOperations,
+        self.uncheckAllButton = createPushButton(filesOperations,
                             i18n="datasource.accept.uncheck.all.button",
                             i18n_def="Uncheck all")
-        self.connect(uncheckAllButton, SIGNAL("clicked()"),
+        self.connect(self.uncheckAllButton, SIGNAL("clicked()"),
                      self.uncheckAllAction)
+
+        self.changeEnablemend(False)
 
     def chooseRootDirAction(self):
         rootDir = QFileDialog.getExistingDirectory(self,
                                     caption=self.chooseRootDirButton.text())
         if rootDir:
+            self.changeEnablemend(True)
+            self.rootDir = rootDir
             self.rootDirLabel.setText(rootDir)
-            self.reloadAction()
+            self.reload()
 
     def checkAllAction(self):
         for idx in range(self.model.rowCount()):
@@ -165,31 +153,72 @@ class ChooseDatasourcePage(QWizardPage):
             self.model.item(idx).setCheckState(Qt.Unchecked)
 
     def filePreviewAction(self):
-        pass
+        if self.selectedRow == None:
+            QMessageBox.information(self, "Information",
+                      "No files selected !")
+        else:
+            model = self.selectedRow.model()
+            path = model.item(self.selectedRow.row(), 2)
+            filename = model.item(self.selectedRow.row(), 0)
+            dialog = FilePreviewDialog(path.text(), filename.text(), self)
+            dialog.exec_()
 
-    def reloadAction(self):
-        _dir = QDir(self.rootDirLabel.text())
-        _dir.setFilter(QDir.Filters(QDir.Files))
-        if len(self.filesExtension.text()) > 0 \
-            and self.filesExtension.text() != "*":
-            _dir.setNameFilters(QStringList(self.filesExtension.text()))
-        infoList = _dir.entryInfoList()
+    def reload(self):
+        if self.rootDir == None:
+            return
+        self.filePreviewButton.setEnabled(False)
+
+        iteratorFlag = QDirIterator.Subdirectories \
+                if self.recursively.checkState() == Qt.Checked \
+                else QDirIterator.NoIteratorFlags
+
+        nameFilters = QStringList(self.filesExtension.text()) \
+                    if len(self.filesExtension.text()) > 0 \
+                        and not self.filesExtension.text() \
+                            in ("*", "*.*", "", None) \
+                    else QStringList()
+
+        dir_iterator = QDirIterator(self.rootDirLabel.text(),
+                                    nameFilters,
+                                    QDir.Filters(QDir.Files),
+                                    iteratorFlag)
         self.model.removeRows(0, self.model.rowCount())
-        for infoFile in infoList:
-            if not (infoFile.isExecutable() or infoFile.size() == 0):
+        while(dir_iterator.next()):
+            infoFile = dir_iterator.fileInfo()
+            if infoFile.isFile() == True:
+                if self.filesExtension.text() in ("*", "*.*", "", None):
+                    if infoFile.isExecutable() or \
+                        is_text_file(infoFile.filePath()) == False:
+                            continue
                 filename = QStandardItem(infoFile.fileName())
                 filename.setCheckState(Qt.Checked)
                 filename.setCheckable(True)
                 size = QStandardItem(str(infoFile.size()))
                 path = QStandardItem(infoFile.path())
                 self.model.appendRow((filename, size, path))
-        if self.model.rowCount() == 0:
-            QMessageBox.critical(self, "Information",
-                      "No files in the directory " + _dir.path())
-        else:
+        if self.model.rowCount() > 0:
             self.filesTableView.resizeColumnsToContents()
             self.filesTableView.scrollToTop()
-            self.filesGroupBox.setEnabled(True)
+
+    def onClickedAction(self, idx):
+        self.selectedRow = idx
+        #row = idx.row()
+        #cols = self.model.columnCount()
+        self.filePreviewButton.setEnabled(True)
+
+    def recursivelyAction(self):
+        self.reload()
+
+    def filesExtensionAction(self, text):
+        self.reload()
+
+    def changeEnablemend(self, enabled):
+        self.filesExtension.setEnabled(enabled)
+        self.recursively.setEnabled(enabled)
+        self.filesTableView.setEnabled(enabled)
+        self.filePreviewButton.setEnabled(enabled)
+        self.checkAllButton.setEnabled(enabled)
+        self.uncheckAllButton.setEnabled(enabled)
 
 
 class ChooseColumnsDataPage(QWizardPage):
@@ -204,3 +233,27 @@ class ChooseColumnsDataPage(QWizardPage):
         layout = QGridLayout()
         layout.addWidget(chooseFileLabel, 0, 0)
         self.setLayout(layout)
+
+
+class FilePreviewDialog(QDialog):
+
+    def __init__(self, path, filename, parent=None):
+        super(FilePreviewDialog, self).__init__(parent)
+        filename = QString(join(str(path), str(filename)))
+        self.setWindowTitle('Preview of ' + filename)
+        self.setGeometry(QRect(50, 50, 1000, 600))
+        self.setLayout(QVBoxLayout())
+        self.lineNumberLabel = createLabel(self)
+        self.preview = createPlainTextEdit(self, readonly=True)
+
+        closeButton = createPushButton(self,
+                            i18n="close",
+                            i18n_def="Close")
+        self.connect(closeButton, SIGNAL("clicked()"), self, SLOT("reject()"))
+        file_ = QFile(QString(join(str(path), str(filename))))
+        if file_.open(QFile.ReadOnly):
+            self.preview.insertPlainText(QString(file_.readAll()))
+            self.preview.moveCursor(QTextCursor.Start)
+            self.lineNumberLabel.setText('Lines # '
+                        + str(self.preview.document().lineCount()))
+            file_.close()
