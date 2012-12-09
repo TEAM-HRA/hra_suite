0e155e2f19cea48b9866e48f893303e0d502ac99
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-12-09 19:57:41 +0000

    add a new class pygui.qt.utils.widgets.WidgetsHorizontalHeader to give ability in QTableView widget to create a header line composed of any widgets not only labels
    git-svn-id: https://jurek-pc3/repositories/doctorate@181 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/utils/widgets.py
+++ PyGUI/src/pygui/qt/utils/widgets.py
@@ -254,3 +254,69 @@ class ProgressBarManager(object):
             if self.isStopped() == False:
                 self.threadTask.emitUpdateTask()
         return not self.isStopped()
+
+
+class WidgetsHorizontalHeader(QHeaderView):
+    """
+    class for table header line used to create a header
+    filled with widgets instead of simple texts
+    """
+    def __init__(self, parent, widgets):
+        super(WidgetsHorizontalHeader, self).__init__(Qt.Horizontal, parent)
+
+        #get optimal size for header line based on sizes of header widgets
+        height = 0
+        width = 0
+        for idx in range(len(widgets)):
+            sizeHint = widgets[idx].sizeHint()
+            if height < sizeHint.height():
+                height = sizeHint.height()
+            if width < sizeHint.width():
+                width = sizeHint.width()
+
+        #very import property used in sizeHint method
+        self.sizeHint = QSize(width + 10, height + 10)
+
+        self.setResizeMode(QHeaderView.Interactive)
+        parent.setHorizontalHeader(self)
+        self.widgets = widgets
+        for idx in range(len(self.widgets)):
+            widgets[idx].setParent(self)
+            x = self.sectionPosition(idx)
+            y = 0
+            w = widgets[idx].sizeHint().width()  # self.sectionSize(idx)
+            h = height
+            widgets[idx].setGeometry(QRect(x, y, w, h))
+            widgets[idx].show()
+
+        #if a header (or section) changes then widgets have to be moved
+        self.connect(self,
+                     SIGNAL("sectionResized(int,int,int)"),
+                     self.sectionResized)
+
+    def sizeHint(self):
+        """
+        very important method without it no widgets are displayed
+        """
+        return self.sizeHint
+
+    def sectionResized(self, logicalIndex, oldSize, newSize):
+        """
+        a section means one header
+        """
+        old = self.widgets[logicalIndex].geometry()
+        self.widgets[logicalIndex].setGeometry(old.x(), old.y(),
+                                               newSize, old.height())
+        #have to move the following headers about difference
+        #between old and new size
+        for idx in range(logicalIndex + 1, len(self.widgets)):
+            self.changeXForHeader(idx, newSize - oldSize)
+
+    def changeXForHeader(self, logicalIndex, x):
+        """
+        parameter x could be positive move to right
+        or negative move to left
+        """
+        old = self.widgets[logicalIndex].geometry()
+        self.widgets[logicalIndex].setGeometry(old.x() + x, old.y(),
+                                            old.width(), old.height())
