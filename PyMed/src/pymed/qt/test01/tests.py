88483360e6f956ad2a8521955e5d1410890a9a2c
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-12-11 17:40:08 +0000

    module for some experimental code
    git-svn-id: https://jurek-pc3/repositories/doctorate@184 13b80749-e00b-5047-ae1c-0d8bb577e5e7

---
+++ PyMed/src/pymed/qt/test01/tests.py
@@ -0,0 +1,41 @@
+'''
+Created on 11-12-2012
+
+@author: jurek
+'''
+
+from PyQt4.QtCore import *  # @UnusedWildImport
+from PyQt4.QtGui import *  # @UnusedWildImport
+from pygui.qt.utils.widgets import createTableView
+from pygui.qt.utils.widgets import WidgetsHorizontalHeader
+
+
+def createTestTable(parentLayout, parent):
+    testTable = createTableView(parent,
+                        selectionBehavior=QAbstractItemView.SelectRows,
+                        selectionMode=QAbstractItemView.SingleSelection)
+    model = QStandardItemModel(parent)
+
+    labels = ["", "", ""]
+    labels = QStringList(labels)
+    model.setHorizontalHeaderLabels(labels)
+    testTable.setModel(model)
+
+    widgets = []
+    for idx in range(3):
+        wid = QWidget()
+        widgets.append(wid)
+        lay = QVBoxLayout()
+        wid.setLayout(lay)
+        bbb = QPushButton("BUT:" + str(idx), wid)
+        lay.addWidget(bbb)
+        bbb = QPushButton("EXTRA:" + str(idx), wid)
+        lay.addWidget(bbb)
+
+    hheader = WidgetsHorizontalHeader(testTable, widgets)
+
+    for idx in range(100):
+        filename = QStandardItem("jeden" + str(idx))
+        size = QStandardItem('two' + str(idx))
+        path = QStandardItem(str(1232132131) + str(idx))
+        model.appendRow((filename, size, path, ))
