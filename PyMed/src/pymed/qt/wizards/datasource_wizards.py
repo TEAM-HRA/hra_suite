388309a1581479ad8aa21a39c19c1b36d4caa6c6
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-12-13 18:32:30 +0000

    move classes pymed.qt.wizards.datasource_wizards.FilesTableView, pymed.qt.wizards.datasource_wizards.CheckStateProxySortFilterModel into module pygui.qt.custom_widgets.modelviews
    git-svn-id: https://jurek-pc3/repositories/doctorate@196 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyMed/src/pymed/qt/wizards/datasource_wizards.py
+++ PyMed/src/pymed/qt/wizards/datasource_wizards.py
@@ -8,7 +8,6 @@ from PyQt4.QtGui import *  # @UnusedWildImport
 from pygui.qt.utils.qt_i18n import QT_I18N
 from pygui.qt.utils.qt_i18n import title_I18N
 from pygui.qt.utils.widgets import createComposite
-from pygui.qt.utils.widgets import createTableView
 from pygui.qt.utils.widgets import createLineEdit
 from pygui.qt.utils.widgets import createCheckBox
 from pygui.qt.utils.widgets import createLabel
@@ -16,12 +15,13 @@ from pygui.qt.utils.widgets import createPushButton
 from pygui.qt.utils.widgets import createGroupBox
 from pygui.qt.utils.graphics import get_width_of_n_letters
 from pycore.io_utils import is_text_file
-from pycore.misc import Params
 from pygui.qt.utils.settings import SettingsFactory
 from pygui.qt.utils.settings import Setter
 from pygui.qt.custom_widgets.separator import SeparatorWidget
 from pygui.qt.custom_widgets.progress_bar import ProgressBarManager
 from pygui.qt.utils.windows import showFilePreviewDialog
+from pygui.qt.custom_widgets.modelviews import FilesTableView
+from pygui.qt.custom_widgets.modelviews import CheckStateProxySortFilterModel
 
 
 class DatasourceWizard(QWizard):
@@ -352,134 +352,3 @@ class ChooseColumnsDataPage(QWizardPage):
 
     def filePreviewAction(self):
         showFilePreviewDialog(self.filesTableView.getSelectedPathAndFilename())
-
-
-class FilesTableView(object):
-    def __init__(self, parent, parent_model, **params):
-        self.parent = parent
-        self.__completed_count__ = 0
-        self.selectedRow = None
-        self.params = Params(**params)
-        if self.params.model:
-            self.model = self.params.model
-        else:
-            self.model = QStandardItemModel(parent_model)
-        self.proxyModel = self.params.proxyModel if self.params.proxyModel \
-                            else None
-        if self.proxyModel:
-            self.proxyModel.setSourceModel(self.model)
-            self.proxyModel.setDynamicSortFilter(True)
-        labels = ["",  # first column is checkable column
-                  QT_I18N("datasource.files.column.filename", "Filename"),
-                  QT_I18N("datasource.files.column.size", "Size"),
-                  QT_I18N("datasource.files.column.path", "File path")]
-        labels = QStringList(labels)
-        self.model.setHorizontalHeaderLabels(labels)
-        self.filesTableView = createTableView(parent,
-                        selectionBehavior=QAbstractItemView.SelectRows,
-                        selectionMode=QAbstractItemView.SingleSelection)
-        if self.proxyModel:
-            self.filesTableView.setModel(self.proxyModel)
-        else:
-            self.filesTableView.setModel(self.model)
-        self.filesTableView.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        if self.params.onClickedAction:
-            self.filesTableView.connect(self.filesTableView,
-                                        SIGNAL('clicked(QModelIndex)'),
-                                        self.params.onClickedAction)
-        if self.params.sorting:
-            self.filesTableView.setSortingEnabled(True)
-
-    def reload(self):
-        if self.model.rowCount() > 0:
-            self.filesTableView.resizeColumnsToContents()
-            self.filesTableView.scrollToTop()
-
-    def addRow(self, row):
-        self.model.appendRow(row)
-
-    def clear(self):
-        self.model.removeRows(0, self.model.rowCount())
-        self.minCompleteState()
-
-    def rowChecked(self, selectedRow):
-        return self.model.item(selectedRow.row()).checkState() == Qt.Checked
-
-    def getSelectedPathAndFilename(self):
-        if not self.selectedRow == None:
-            model = self.selectedRow.model()
-            path = model.item(self.selectedRow.row(), 3)
-            filename = model.item(self.selectedRow.row(), 1)
-            return (path.text(), filename.text())
-
-    def onClickedAction(self, selectedRow):
-        self.selectedRow = selectedRow
-        checked = self.rowChecked(selectedRow)
-        self.changeCompleteState(1, 'add' if checked else 'sub')
-
-    def getSelectedItems(self):
-        return [self.model.item(row)
-                for row in range(0, self.model.rowCount())
-                    if self.model.item(row).checkState() == Qt.Checked]
-
-    def setCheckedRowState(self, idx, state):
-        self.model.item(idx).setCheckState(state)
-
-    def getRowCount(self):
-        return self.model.rowCount()
-
-    def setEnabled(self, enabled):
-        self.filesTableView.setEnabled(enabled)
-
-    def resizeColumnsToContents(self):
-        self.filesTableView.resizeColumnsToContents()
-
-    def changeCompleteState(self, value, operation='set'):
-        """
-        method used to emit a signal completeChanged() which is intercepted
-        by QWizard to enable/disable next, previous buttons based on value
-        returned by isComplete method of a wizard page object
-        correction:
-        it's better do not send a completeChange signal, because
-        program jump to the beginning of a table view instead of sticking
-        to the position where it is already
-        """
-        if operation == 'set':
-            self.__completed_count__ = value
-        elif operation == 'add':
-            self.__completed_count__ = self.__completed_count__ + value
-        elif operation == 'sub':
-            if self.__completed_count__ - value >= 0:
-                self.__completed_count__ = self.__completed_count__ - value
-
-        #self.emit(SIGNAL("completeChanged()"))
-        if self.params.wizardButtons:
-            for button in self.params.wizardButtons:
-                self.params.wizard.button(button).setEnabled(
-                                                self.__completed_count__ > 0)
-
-    def getCompletedCount(self):
-        return self.__completed_count__ > 0
-
-    def maxCompleteState(self):
-        self.changeCompleteState(self.getRowCount())
-
-    def minCompleteState(self):
-        self.changeCompleteState(0)
-
-    def getModel(self):
-        return self.model
-
-    def setColumnHidden(self, column, hide=True):
-        self.filesTableView.setColumnHidden(column, hide)
-
-
-class CheckStateProxySortFilterModel(QSortFilterProxyModel):
-    def __init__(self, parent=None):
-        super(CheckStateProxySortFilterModel, self).__init__(parent)
-
-    def filterAcceptsRow(self, source_row, source_parent):
-        return self.sourceModel().item(source_row).checkState() == Qt.Checked # @IgnorePep8
-
-    def item(self, row, column=0):
-        return self.sourceModel().item(row, column)
