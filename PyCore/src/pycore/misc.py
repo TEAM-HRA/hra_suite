1132d61c336421e8ba294f4d7bf3f8378c40943c
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-12-13 11:30:46 +0000

    add new methods: def get_separator_between_numbers(_string) def contains_letter(_string)
    git-svn-id: https://jurek-pc3/repositories/doctorate@186 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyCore/src/pycore/misc.py
+++ PyCore/src/pycore/misc.py
@@ -3,6 +3,8 @@ Created on 03-12-2012
 
 @author: jurek
 '''
+from re import findall
+from re import search
 
 
 class Params(object):
@@ -18,3 +20,23 @@ class Params(object):
     # if parameter is not set in the __init__() method then returns None
     def __getattr__(self, name):
         return None
+
+
+def get_separator_between_numbers(_string):
+    """
+    function get a separator between numbers in a string;
+    at first step the function fetches all non number elements from the string
+    into a list, in the second step compares a string made by multiplication
+    of the first element in the list by number of list elements with
+    a string made by joining all elements of the list, if these two strings
+    are equal then there is only one separator in the string
+    """
+    not_numbers = findall(r'[^0-9\.]+', _string)
+    return not_numbers[0] \
+        if len(not_numbers) > 0 and \
+            not_numbers[0] * len(not_numbers) == "".join(not_numbers) \
+        else None
+
+
+def contains_letter(_string):
+    return not search(r'[a-zA-Z]+', _string) == None
