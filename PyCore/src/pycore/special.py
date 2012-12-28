3bc43972f7c4b09d63a6005eec70dcc8114039ed
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2012-12-28 17:04:14 +0000

    add displaying of module name in the case of import error
    git-svn-id: https://jurek-pc3/repositories/doctorate@249 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyCore/src/pycore/special.py
+++ PyCore/src/pycore/special.py
@@ -5,7 +5,7 @@ Created on 24-12-2012
 '''
 
 
-def ImportErrorMessage(importError):
+def ImportErrorMessage(importError, name=None):
     """
     method which dynamically show Tk window
     with import message error
@@ -14,7 +14,8 @@ def ImportErrorMessage(importError):
 from Tkinter import *
 root = Tk()
 message = '""" + importError.message + """\\n' + \\
-         'Check if it is a hidden import during build process'
+         'Check if it is a hidden import during build process\\n' + \\
+         """ + ("" if name == None else "'[module: " + name + "]'") + """
 w = Label(root, text=message)
 w.pack()
 root.mainloop()
