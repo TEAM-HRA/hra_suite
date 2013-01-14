75508e1fca495fc59920607f81d403844739824f
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-01-14 19:04:55 +0000

    add a method: pygui.qt.models.datasources.DatasourceFilesSpecificationModel.fileSpecification(self, row) which returns a named tuple: pygui.qt.models.datasources.DatasourceFilesSpecificationModel (consists of the following fields filepath, filename, data_index, annotation_index, separator) for specified row number
    git-svn-id: https://jurek-pc3/repositories/doctorate@280 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/models/datasources.py
+++ PyGUI/src/pygui/qt/models/datasources.py
@@ -3,22 +3,29 @@ Created on 02-01-2013
 
 @author: jurek
 '''
+import collections
 from PyQt4.QtCore import *  # @UnusedWildImport
 from PyQt4.QtGui import *  # @UnusedWildImport
 from pycore.collections import empty_string
 
 
 class DatasourceFilesSpecificationModel(QStandardItemModel):
+
+    labels = ["filepath", "filename", "data_index", "annotation_index", "separator"] # @IgnorePep8
+    FileSpecification = collections.namedtuple('FileSpecification', labels)
+
     def __init__(self):
         QStandardItemModel.__init__(self)
-        labels = ["path", "filename", "data_index",
-                  "annotation_index", "separator"]
-        self.setHorizontalHeaderLabels(labels)
+        self.setHorizontalHeaderLabels(DatasourceFilesSpecificationModel.labels) # @IgnorePep8
 
     def appendRow(self, _path, _filename, _dataIndex, _annotationIndex,
                   _separator):
-
         row = [QStandardItem(QString(empty_string(item)))
                 for item in [_path, _filename, _separator, _dataIndex,
                              _annotationIndex]]
         super(DatasourceFilesSpecificationModel, self).appendRow(row)
+
+    def fileSpecification(self, row):
+        data = [self.item(row, column).text()
+                for column in range(self.columnCount())]
+        return DatasourceFilesSpecificationModel.FileSpecification(*data)
