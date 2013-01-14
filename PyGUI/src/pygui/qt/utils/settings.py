6e1ee8a19922be20f2752958c94970b1783940e0
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-01-14 19:18:18 +0000

    changes a class pygui.qt.utils.settings.Setter a) add the following properties: objectName - a name added to identifier of a settings item _conv_2level - the second conversion filter invoked after he first passed in _conv parameter _no_conv - indicate not to run a filter, also not to run the default one as the first filter b) add a method: pygui.qt.utils.settings.Setter.__id(self, prefix) to extract code for creation of identifier of a settings item c) if a value returned from settings storage is a tuple, as a outcome for example of QVariant.toInt conversion method, it is assumed that the value of the first element is a proper value of setting's item
    git-svn-id: https://jurek-pc3/repositories/doctorate@281 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/utils/settings.py
+++ PyGUI/src/pygui/qt/utils/settings.py
@@ -28,38 +28,58 @@ class SettingsFactory(object):
 
     @staticmethod
     def _getInitials(_target, **params):
-        return (QSettings(), params.get('_prefix', _target.__class__.__name__))
+        return (QSettings("med", "med"),
+                params.get('_prefix', _target.__class__.__name__))
 
 
 class Setter(object):
     def __init__(self, **params):
         self.__conv = params.get('_conv', None)
-        self.__name = get_other_keys(params, ('_conv',))
+        self.__no_conv = params.get('_no_conv', False)
+        self.__name = get_other_keys(params, ('_conv', 'objectName',
+                                              '_conv_2level', '_no_conv'))
         self.__value = params[self.__name]
+        self.__object_name = params.get('objectName', '')
+        self.__conv_2level = params.get('_conv_2level', None)
 
     def get(self, _prefix, _settings):
-        return _settings.value(_prefix + '/' + self.__name,
+        return _settings.value(self.__id(_prefix),
                 QVariant(self.__value) if self.__value else QVariant())
 
     def set(self, _prefix, _settings):
-        value = eval(self.__conv.__name__ + '(' + repr(self.__value) + ')') \
-                if self.__conv and self.__value else self.__value
-        _settings.setValue(_prefix + '/' + self.__name,
-                    QVariant(value) if value else QVariant())
+        if self.__no_conv == True:
+            _settings.setValue(self.__id(_prefix), QVariant(self.__value))
+        else:
+            value = eval(self.__conv.__name__ + '(' + repr(self.__value) + \
+                         ')') if self.__conv and self.__value else self.__value
+            _settings.setValue(self.__id(_prefix),
+                               QVariant(value) if value else QVariant())
 
     def load(self, _target, _prefix, _settings):
-        if not self.__conv:
+        if not self.__conv and self.__no_conv == False:
             self.__conv = QVariant.toString
         value = self.get(_prefix, _settings)
-        if self.__conv:  # invoke a conversion
+        if self.__conv and self.__no_conv == False:  # invoke a conversion
             value = eval('value.' + self.__conv.__name__ + '()')
+
+            if isinstance(value, tuple):
+                #this means that a tuple has been returned and a real value
+                #is stored in the first element, this happens for some
+                #converters for example: (int, bool ok) QVariant.toInt(self)
+                value = value[0]
         # check if self.name exists and it is a method or something callable
         if self.__name in dir(_target) and hasattr(
                             eval('_target.' + self.__name), '__call__'):
             eval('_target.' + self.__name + '(value)')
         else:
             _target.__dict__[self.__name] = value
+            if self.__conv_2level:
+                value = self.__conv_2level(value)
+                _target.__dict__[self.__name] = value
         return value
 
     def save(self, _prefix, _settings):
         self.set(_prefix, _settings)
+
+    def __id(self, _prefix):
+        return self.__object_name + '/' + _prefix + '/' + self.__name
