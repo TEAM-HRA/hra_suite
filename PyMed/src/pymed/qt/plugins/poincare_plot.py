7054501402313f818d188bb5c65e1c2ccaf4e52b
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-01-14 20:52:34 +0000

    further enhancements in pymed.qt.plugins.poincare_plot.PoincareTabWidget class, the main widget in PoincareTabWidget is a splitter which includes two parts (at this moment): - datasource composite with datasource files items acquired from datasource wizard - plot composite - a main widget for plotting stuff
    git-svn-id: https://jurek-pc3/repositories/doctorate@287 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyMed/src/pymed/qt/plugins/poincare_plot.py
+++ PyMed/src/pymed/qt/plugins/poincare_plot.py
@@ -6,18 +6,57 @@ Created on 05-01-2013
 from PyQt4.QtCore import *  # @UnusedWildImport
 from PyQt4.QtGui import *  # @UnusedWildImport
 from pycore.misc import Params
+from pygui.qt.utils.widgets import createSplitter
+from pygui.qt.utils.widgets import createListWidget
+from pygui.qt.utils.widgets import createWidget
+from pygui.qt.models.datasources import DatasourceFilesSpecificationModel
 
 
 class PoincareTabWidget(QWidget):
-    """
-    only for test purpose
-    """
+
     def __init__(self, **params):
         self.params = Params(**params)
-        QWidget.__init__(self, parent=self.params.parent)
-        #self.setParent(_parent)
-        self.setLayout(QHBoxLayout())
-        #_parent.layout().addWidget(composite)
+        super(PoincareTabWidget, self).__init__(self.params.parent)
+        layout = QHBoxLayout()
+        self.setLayout(layout)
+        self.__splitter__ = createSplitter(self, objectName='poincarePlot',
+                                           save_state=True)
+        self.__createDatasourceComposite__()
+        self.__createPlotComposite__()
 
-        clearButton = QPushButton("Clear", self)
-        self.layout().addWidget(clearButton)
+        #this method's call is very important, it sets up widgets sizes
+        #which make up a splitter;
+        #it has to be the last operation in the PoincareTabWidget's
+        #creation process
+        self.__splitter__.updateSizes()
+
+    def closeTab(self):
+        """
+        this method includes actions to be invoked when a PoincareTabWidget is closing @IgnorePep8
+        """
+        self.__splitter__.destroySplitter()
+
+    def __createDatasourceComposite__(self):
+        self.__datasourceComposite__ = createWidget(self.__splitter__,
+                                                    add_widget_to_parent=True)
+        self.__datasourceList__ = createListWidget(
+                                                self.__datasourceComposite__)
+        model = self.params.model
+        if model and \
+            isinstance(model, DatasourceFilesSpecificationModel):
+            for row in range(model.rowCount()):
+                QListWidgetItem(model.fileSpecification(row).filename,
+                            self.__datasourceList__)
+        else:
+            QListWidgetItem('model not specified or incorrect type',
+                            self.__datasourceList__)
+        if self.__splitter__.sizesLoaded() == False:
+            idx = self.__splitter__.indexOf(self.__datasourceComposite__)
+            self.__splitter__.setStretchFactor(idx, 1)
+
+    def __createPlotComposite__(self):
+        self.__plotComposite__ = createWidget(self.__splitter__,
+                                              add_widget_to_parent=True)
+        if self.__splitter__.sizesLoaded() == False:
+            idx = self.__splitter__.indexOf(self.__plotComposite__)
+            self.__splitter__.setStretchFactor(idx, 20)
