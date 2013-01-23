cd743f6d79e42410066399dadfa11f336d6912ad
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-01-23 22:27:04 +0000

    enhancements in activities mechanism: a) add a new class pygui.qt.activities.activities.ActivityManager used to manage activities objects add, get or remove
    b) change in class
    pygui.qt.activities.activities.ActivityWidget
    method
    def activityDescription(self)
    into
    def description(self)
    
    c) add a new widget 
    class ActivityDockWidget(DockWidgetCommon)
    used in QMainWindow composite as a kind of dock widget with a list of all activities
    
    d) add a new class
    class Activity(object)
    as a base class for all activities
    
    e) add a new class 
    class PluginActivity(Activity)
    used as a activity associated with plugins mechanism;
    invoking an object of PluginActivity class is equivalent of invoking a plugin functionality
    
    git-svn-id: https://jurek-pc3/repositories/doctorate@352 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/activities/activities.py
+++ PyGUI/src/pygui/qt/activities/activities.py
@@ -5,14 +5,33 @@ Created on 22-01-2013
 '''
 from pycore.special import ImportErrorMessage
 try:
+    import datetime
+    import inspect
+    from pycore.misc import replace_all
     from PyQt4.QtCore import *  # @UnusedWildImport
     from PyQt4.QtGui import *  # @UnusedWildImport
+    from pycore.collections import get_subdict
+    from pygui.qt.utils.settings import SettingsFactory
     from pygui.qt.utils.widgets import CompositeCommon
     from pygui.qt.utils.widgets import CheckBoxCommon
     from pygui.qt.utils.widgets import LabelCommon
     from pygui.qt.utils.widgets import LineEditCommon
+    from pygui.qt.utils.widgets import DockWidgetCommon
+    from pygui.qt.utils.widgets import ListWidgetCommon
+    from pygui.qt.utils.widgets import ListWidgetItemCommon
 except ImportError as error:
     ImportErrorMessage(error, __name__)
+
+
+class ActivityManager(QObject):
+    @staticmethod
+    def saveActivity(activity):
+        SettingsFactory.saveObject(activity.activity_id, activity)
+
+    @staticmethod
+    def getActivities(activity_group):
+        return SettingsFactory.getObjectsForGroup('/'.join(['activity',
+                                                     activity_group]))
 
 
 class ActivityWidget(CompositeCommon):
@@ -36,9 +55,98 @@ class ActivityWidget(CompositeCommon):
     def activityOk(self):
         return self.activityButton.isChecked()
 
-    def activityDescription(self):
+    def description(self):
         if self.activityButton.isChecked():
             return str(self.activityDescription.text())
 
     def __clickedHandler__(self):
         self.activityDescription.setEnabled(self.activityButton.isChecked())
+
+
+class ActivityDockWidget(DockWidgetCommon):
+    """
+    a dock widget for activities
+    """
+    def __init__(self, parent, **params):
+        super(ActivityDockWidget, self).__init__(parent,
+                                    title=params.get('title', 'Activities'),
+                                    **params)
+        self.setObjectName("ActivityDockWidget")
+        self.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+        self.listWidget = ListWidgetCommon(self,
+                not_add_widget_to_parent_layout=True,
+                list_item_clicked_handler=self.__list_item_handler__)
+        for activity in ActivityManager.getActivities(PLUGIN_ACTIVITY_TYPE):
+            ListWidgetItemCommon(self.listWidget,
+                                 text=activity.label,
+                                 data=activity)
+        self.setWidget(self.listWidget)
+        parent.addDockWidget(Qt.RightDockWidgetArea, self)
+
+    def __list_item_handler__(self, listItem):
+        data = listItem.data(Qt.UserRole)
+        if data:
+            activity = data.toPyObject()
+            if activity:
+                activity()
+
+
+PLUGIN_ACTIVITY_TYPE = 'plugin'
+
+
+class Activity(object):
+    def __init__(self, activity_type, **params):
+        self.__activity_type__ = activity_type
+        self.__params__ = params
+        self.__date_time__ = datetime.datetime.now()
+        self.__description__ = params.get('description', '')
+        self.__activity_id__ = '/'.join(['activity', self.activity_type,
+                '_'.join([self.description_as_id, self.date_and_time_as_id])])
+
+    @property
+    def params(self):
+        return self.__params__
+
+    @property
+    def date_and_time_as_string(self):
+        return self.__date_time__.strftime('%Y-%m-%d %H:%M:%S')
+
+    @property
+    def date_and_time_as_id(self):
+        return replace_all(self.date_and_time_as_string, '_', ['-', ' ', ':'])
+
+    @property
+    def activity_id(self):
+        return self.__activity_id__
+
+    @property
+    def activity_type(self):
+        return self.__activity_type__
+
+    @property
+    def label(self):
+        return ' '.join([self.description, self.date_and_time_as_string])
+
+    @property
+    def description(self):
+        return self.__description__
+
+    @property
+    def description_as_id(self):
+        return replace_all(self.description, '_', ['-', ' ', ':', '/', ','])
+
+
+class PluginActivity(Activity):
+    def __init__(self, _plugin_id, **params):
+        super(PluginActivity, self).__init__(PLUGIN_ACTIVITY_TYPE, **params)
+        self.__plugin_id__ = _plugin_id
+
+    @property
+    def plugin_id(self):
+        return self.__plugin_id__
+
+    def __call__(self):
+        #this explicit import is required here to prevent from a cyclic import
+        from pygui.qt.utils.plugins import PluginsManager
+        PluginsManager.invokePlugin(self.__plugin_id__, inspect.stack(),
+                        **get_subdict(self.params, not_keys=['description']))
