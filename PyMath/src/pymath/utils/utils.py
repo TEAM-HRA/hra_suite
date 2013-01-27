0b6d850e248a4dd6a464d07ab3a5d95eee531054
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-01-27 14:57:27 +0000

    1) remove use of optparse module 2) add a tool class pymath.utils.utils.Params
    git-svn-id: https://jurek-pc3/repositories/doctorate@383 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyMath/src/pymath/utils/utils.py
+++ PyMath/src/pymath/utils/utils.py
@@ -3,8 +3,15 @@ Created on 26-01-2013
 
 @author: jurek
 '''
-import inspect
-import optparse
+
+
+def print_import_error(_module, _error):
+    print('import error: ' + str(_error) + ' in module: ' + str(_module))
+
+try:
+    import inspect
+except ImportError as error:
+    print_import_error(__name__, error)
 
 
 def get_values_as_map(_object, with_none=False):
@@ -18,11 +25,20 @@ def get_members_names(_object_class):
                     if not method.startswith('__')]
 
 
-def print_import_error(_module, _error):
-    pass
+#this variable is set up by external code, too
+USE_NUMPY_EQUIVALENT = True
 
-__parser = optparse.OptionParser()
-__parser.add_option("-n", "--use_numpy_equivalent", default=True)
-__opts, __args = __parser.parse_args()
 
-USE_NUMPY_EQUIVALENT = __opts.numpy_equivalent
+class Params(object):
+    """
+    class which represents dictionary parameters
+    by object where elements are accessible with dot notation
+    if client tries to access not existing element then None value is returned
+    """
+    def __init__(self, **params):
+        for param in params:
+            setattr(self, param, params[param])
+
+    # if parameter is not set in the __init__() method then returns None
+    def __getattr__(self, name):
+        return None
