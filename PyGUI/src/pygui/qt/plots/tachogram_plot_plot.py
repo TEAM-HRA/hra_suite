c57bc66a713695dba19ee12dadeeb580e5aebd6e
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-02-03 21:37:21 +0000

    add new icons: graph_button=@.settings\\icons\\graph_button.ico scatter_plot_button=@.settings\\icons\\scatter_plot_button.ico use in a tool button in a tachogram plot
    add some test code in tachogram plot class
    
    git-svn-id: https://jurek-pc3/repositories/doctorate@404 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/plots/tachogram_plot_plot.py
+++ PyGUI/src/pygui/qt/plots/tachogram_plot_plot.py
@@ -4,6 +4,9 @@ Created on 15-01-2013
 @author: jurek
 '''
 from pycore.special import ImportErrorMessage
+from pygui.qt.utils.widgets import PushButtonCommon
+from pycommon.actions import ActionSpec
+from pygui.qt.actions.actions_utils import create_action
 try:
     from PyQt4.QtGui import *  # @UnusedWildImport
     from PyQt4.QtCore import *  # @UnusedWildImport
@@ -35,7 +38,7 @@ class TachogramPlotPlot(CompositeCommon):
                                     signal=data_source.signal,
                                     annotation=data_source.annotation)
         layout.addWidget(self.canvas)
-        self.navigation_toolbar = NavigationToolbar(self.canvas, self)
+        self.navigation_toolbar = TachogramNavigationToolbar(self.canvas, self)
         layout.addWidget(self.navigation_toolbar)
 
 
@@ -53,9 +56,53 @@ class TachogramPlotCanvas(FigureCanvas):
         #self.y = np.cos(2 * np.pi * self.x)
         self.x = np.arange(0.0, len(self.params.signal), 1)
         self.y = self.params.signal
-        self.axes.plot(self.x, self.y)
+        self.axes.scatter(self.x, self.y)
+        #self.axes.plot(self.x, self.y)
+        #self.axes.plot(self.y)
         FigureCanvas.__init__(self, self.fig)
         self.setParent(parent)
+
         FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding,
                                     QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
+
+
+class TachogramNavigationToolbar(NavigationToolbar):
+
+    def __init__(self, canvas, parent):
+        # create the default toolbar
+        NavigationToolbar.__init__(self, canvas, parent)
+        self.canvas = canvas
+        # add new toolbar buttons
+
+        normal_plot_action = self.__createAction__(title="Normal plot",
+                                            handler=self.__normalPlot__,
+                                            iconId='graph_button')
+        self.addAction(normal_plot_action)
+
+        scatter_plot_action = self.__createAction__(title="Scatter plot",
+                                            handler=self.__scatterlPlot__,
+                                            iconId='scatter_plot_button')
+        self.addAction(scatter_plot_action)
+
+    def __normalPlot__(self):
+        self.canvas.axes.cla()
+        self.canvas.axes.plot(self.canvas.x, self.canvas.y)
+        self.canvas.draw()
+
+    def __scatterlPlot__(self):
+        self.canvas.axes.cla()
+        self.canvas.axes.scatter(self.canvas.x, self.canvas.y)
+        self.canvas.draw()
+
+    def __createAction__(self, **params):
+        return create_action(self.parent(), ActionSpec(**params))
+
+#    def __init2__(self, **params):
+#        self.params = Params(**params)
+#        self.__iconId = self.params.iconId
+#        self.__tipId = self.params.tipId
+#        self.__signal = self.params.signal
+#        self.__slot = self.params.handler
+#        self.__title = self.params.title
+#        self.__checkable = ("True" == str(self.params.checkable))
