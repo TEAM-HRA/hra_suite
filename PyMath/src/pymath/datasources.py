7dab7ec649dde432abdb9a83641c23cd077deb5a
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-02-23 21:53:33 +0000

    in a module pymath.datasources: a) add new functions:
    def exclude_boundary_annotations(_signal, _annotation, _excluded_annotations)
        method removes boundary annotations from _annototion array and
        corresponding items from _signal array, returns named tuple
        SignalNoBoundaryAnnotation which includes new signal, new annotation
        array and indexes of remaining annotation values from _annontation
        array; what values are annotations is defined by _excluded_annotations
    
    
    def get_annotation_indexes(_annotation, _excluded_annotations)
        method returns indexes of annotation's values in _annotation parameter
        according to annotations defined by _excluded_annotations parameter
    
    
    def get_not_annotation_indexes(_annotation, _excluded_annotations)
        method returns indexes of not annotation's values in _annotation parameter,
        annotation values are defined in _excluded_annotations parameter
    
    b) change numpy import into pylab import
    
    git-svn-id: https://jurek-pc3/repositories/doctorate@458 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyMath/src/pymath/datasources.py
+++ PyMath/src/pymath/datasources.py
@@ -6,13 +6,81 @@ Created on 15-08-2012
 from pymath.utils.utils import print_import_error
 try:
     import os
-    import numpy as np
+    import collections
+    import pylab as pl
     from re import compile
     from pycore.misc import Params
     from pycore.units import Millisecond
     from pymath.utils.utils import USE_NUMPY_EQUIVALENT
 except ImportError as error:
     print_import_error(__name__, error)
+
+
+ALL_ANNOTATIONS = -1
+
+SignalNoBoundaryAnnotation = \
+    collections.namedtuple('SignalNoBoundaryAnnotation',
+                           ["signal", "annotation", "annotation_indexes"])
+
+
+def exclude_boundary_annotations(_signal, _annotation, _excluded_annotations):
+    """
+    method removes boundary annotations from _annototion array and
+    corresponding items from _signal array, returns named tuple
+    SignalNoBoundaryAnnotation which includes new signal, new annotation
+    array and indexes of remaining annotation values from _annontation
+    array; what values are annotations is defined by _excluded_annotations
+    parameter
+    """
+    if _annotation == None or \
+        pl.sum(_annotation, dtype=int) == 0:
+        return SignalNoBoundaryAnnotation(_signal, _annotation, None)
+
+    #removing nonsinus beats from the beginning
+    while (_annotation[0] != 0
+            and (_excluded_annotations == ALL_ANNOTATIONS
+                 or _annotation[0] in _excluded_annotations)):
+        _signal = _signal[1:]
+        _annotation = _annotation[1:]
+
+    #removing nonsinus beats from the end
+    while (_annotation[-1] != 0
+            and (_excluded_annotations == ALL_ANNOTATIONS
+                or _annotation[-1] in _excluded_annotations)):
+        _signal = _signal[:-1]
+        _annotation = _annotation[:-1]
+
+    annotation_indexes = get_annotation_indexes(_annotation,
+                                                _excluded_annotations)
+    return SignalNoBoundaryAnnotation(_signal, _annotation, annotation_indexes)
+
+
+def get_annotation_indexes(_annotation, _excluded_annotations):
+    """
+    method returns indexes of annotation's values in _annotation parameter
+    according to annotations defined by _excluded_annotations parameter
+    """
+    if _excluded_annotations == ALL_ANNOTATIONS:
+        return pl.array(pl.find(_annotation != 0))
+    else:
+        #find indexes of annotation array where values are in
+        #_excluded_annotations list
+        return pl.array(pl.where(
+                    pl.in1d(_annotation, _excluded_annotations))[0], dtype=int)
+
+
+def get_not_annotation_indexes(_annotation, _excluded_annotations):
+    """
+    method returns indexes of not annotation's values in _annotation parameter,
+    annotation values are defined in _excluded_annotations parameter
+    """
+    if _excluded_annotations == ALL_ANNOTATIONS:
+        return pl.array(pl.find(_annotation == 0))
+    else:
+        #find indexes of an annotation array which are NOT included
+        #in _excluded_annotations list
+        return pl.array(pl.where(pl.logical_not(
+                pl.in1d(_annotation, _excluded_annotations)))[0], dtype=int)
 
 
 class DataVector(object):
@@ -174,11 +242,11 @@ class FileDataSource(object):
                 for index in self.__cols__:
                     _data[index].append(contents[self.__cols__[index]])
 
-        return self.__createData__(map(np.array, _data))
+        return self.__createData__(map(pl.array, _data))
 
     def __getNumPyData__(self):
         _data = [None] * len(self.__cols__)
-        _data = np.loadtxt(self.__file__,
+        _data = pl.loadtxt(self.__file__,
                         skiprows=len(self.headers),
                         usecols=self.__cols__,
                         unpack=True,
@@ -209,5 +277,5 @@ class FileDataSource(object):
     def getUniqueAnnotations(self):
         data = self.getData()
         if data.annotation is not None:
-            unique_annotations = np.unique(data.annotation)
-            return unique_annotations[np.where(unique_annotations > 0)]
+            unique_annotations = pl.unique(data.annotation)
+            return unique_annotations[pl.where(unique_annotations > 0)]
