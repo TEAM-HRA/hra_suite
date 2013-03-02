b330847430d3c71fd36ad8357759e8997d9f834f
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-03-02 18:05:35 +0000

    add a new function in module pymath.utils.array_utils: def get_max_index_for_cumulative_sum_of_means_greater_then_value(_array, _value)     this function returns the first index of array calculated as array of     mean values of the parameter _array at position where cumulative sum     of the calculated array is grater or equal then parameter _value
    git-svn-id: https://jurek-pc3/repositories/doctorate@473 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyMath/src/pymath/utils/array_utils.py
+++ PyMath/src/pymath/utils/array_utils.py
@@ -18,3 +18,19 @@ def get_max_index_for_cumulative_sum_greater_then_value(_array, value,
     cumulative_array = np.cumsum(sub_array)
     indexes = np.where(cumulative_array >= value)[0]
     return indexes[0] + start_index if len(indexes) > 0 else -1
+
+
+def get_max_index_for_cumulative_sum_of_means_greater_then_value(_array, _value): # @IgnorePep8
+    """
+    this function returns the first index of array calculated as array of
+    mean values of the parameter _array at position where cumulative sum
+    of the calculated array is grater or equal then parameter _value
+    """
+    mean_array = np.array([np.mean(_array)] * len(_array))
+    return get_max_index_for_cumulative_sum_greater_then_value(mean_array,
+                                                               _value)
+
+#test code
+if __name__ == '__main__':
+    a = np.array([1, 2, 1, 3, 2, 1, 2, 3, 2, 1, 1, 2])
+    print(str(get_max_index_for_cumulative_sum_of_means_greater_then_value(a, 7)))  # @IgnorePep8
