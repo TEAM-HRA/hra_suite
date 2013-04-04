38d9ba7b6db04d20fcfbca67693b9cb199291fe8
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-04-04 20:57:42 +0000

    extract from module pygui.qt.plots.tachogram_plot_settings_dock_widget classes concerning tachogram plot statistics dock widget, that is the following classes: TachogramPlotStatisticsDockWidget TachogramStatisticsWidget TachogramStatisticsModel into new pygui.qt.plots.tachogram_plot_statistics_dock_widget
    git-svn-id: https://jurek-pc3/repositories/doctorate@553 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/plots/tachogram_plot.py
+++ PyGUI/src/pygui/qt/plots/tachogram_plot.py
@@ -9,25 +9,18 @@ try:
     from PyQt4.QtCore import *  # @UnusedWildImport
     from pycore.misc import Params
     from pycore.introspection import get_child_of_type
-    from pymath.datasources import FileDataSource
-    from pymath.statistics.tachogram_statistics import calculate_tachogram_statistics # @IgnorePep8
-    from pygui.qt.utils.dnd import CopyDragger
     from pygui.qt.utils.widgets import MainWindowCommon
     from pygui.qt.utils.widgets import LabelCommon
-    from pygui.qt.utils.widgets import DockWidgetCommon
-    from pygui.qt.utils.widgets import CompositeCommon
-    from pygui.qt.utils.widgets import TableViewCommon
     from pygui.qt.custom_widgets.toolbars import OperationalToolBarWidget
     from pygui.qt.custom_widgets.tabwidget import TabWidgetCommon
     from pygui.qt.plots.tachogram_plot_plot import TachogramPlotPlot
-    from pygui.qt.plots.tachogram_plot_plot import STATISTIC_MIME_ID
-    from pygui.qt.plots.tachogram_plot_plot import STATISTIC_CLASS_NAME_ID
     from pygui.qt.utils.signals import SignalDispatcher
     from pygui.qt.utils.signals import TAB_WIDGET_ADDED_SIGNAL
     from pygui.qt.plots.plots_signals import CLOSE_TACHOGRAM_PLOT_SIGNAL
     from pygui.qt.plots.plots_signals import MAXIMIZE_TACHOGRAM_PLOT_SIGNAL
     from pygui.qt.plots.plots_signals import RESTORE_TACHOGRAM_PLOT_SIGNAL
-    from pygui.qt.plots.tachogram_plot_settings_dock_widget import TachogramPlotSettingsDockWidget # @IgnorePep8    
+    from pygui.qt.plots.tachogram_plot_settings_dock_widget import TachogramPlotSettingsDockWidget # @IgnorePep8
+    from pygui.qt.plots.tachogram_plot_statistics_dock_widget import TachogramPlotStatisticsDockWidget  # @IgnorePep8
 except ImportError as error:
     ImportErrorMessage(error, __name__)
 
@@ -126,86 +119,3 @@ class TachogramPlotWindow(MainWindowCommon):
 #                    i18n_def="STATUS",
 #                    add_widget_to_parent=True)
 #
-
-
-class TachogramStatisticsWidget(TableViewCommon):
-    """
-    a widget to display basic tachogram's statistics
-    """
-    def __init__(self, parent, **params):
-        TableViewCommon.__init__(self, parent, **params)
-        self.__dragger__ = CopyDragger(self, STATISTIC_MIME_ID, drag_only=True)
-        self.setSelectionMode(QAbstractItemView.SingleSelection)
-        self.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.setColumnHidden(0, True)  # "classname" columns is hidden
-        self.__createModel__()
-
-    def __createModel__(self):
-        model = TachogramStatisticsModel(self)
-        labels = QStringList(["class_name", "Statistic", "Value"])
-        model.setHorizontalHeaderLabels(labels)
-        self.setModel(model)
-
-    def setTachogramStatistics(self, _statistics):
-        model = self.model()
-        model.removeRows(0, model.rowCount())
-        values = _statistics[0]
-        descriptions = _statistics[1]
-        self.class_names = sorted([name for name in values])
-        for name in sorted([name for name in self.class_names]):
-            model.appendRow([QStandardItem(str(name)),
-                             QStandardItem(str(descriptions[name])),
-                             QStandardItem(str(values[name]))])
-
-    def startDrag(self, dropActions):
-        row = self.model().itemFromIndex(self.currentIndex()).row()
-        self.__dragger__.clear()
-        self.__dragger__.dragObject(STATISTIC_CLASS_NAME_ID,
-                                    self.class_names[row])
-        self.__dragger__.startDrag()
-
-
-class TachogramStatisticsModel(QStandardItemModel):
-    def __init__(self, parent):
-        QStandardItemModel.__init__(self, parent=parent)
-
-    def data(self, _modelIndex, _role):
-        #the first column (with index 0) is a name of statistic
-        if _modelIndex.column() == 1 and _role == Qt.TextAlignmentRole:
-            return Qt.AlignRight
-        else:
-            return super(TachogramStatisticsModel, self).data(_modelIndex,
-                                                              _role)
-
-
-class TachogramPlotStatisticsDockWidget(DockWidgetCommon):
-    """
-    a dock widget for tachogram plot statistics
-    """
-    def __init__(self, parent, **params):
-        self.params = Params(**params)
-        super(TachogramPlotStatisticsDockWidget, self).__init__(parent,
-                        title=params.get('title', 'Tachogram plot statistics'),
-                        **params)
-        file_data_source_params = self.params.file_specification._asdict()
-        file_data_source = FileDataSource(**file_data_source_params)
-        data_vector = file_data_source.getData()
-
-        self.setObjectName("TachogramPlotStatisticsDockWidget")
-        self.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea |
-                             Qt.TopDockWidgetArea | Qt.BottomDockWidgetArea)
-        layout = QVBoxLayout()
-        layout.setMargin(0)  # no margin for internal layout
-        self.dockComposite = CompositeCommon(self, layout=layout,
-                                        not_add_widget_to_parent_layout=True)
-        self.__createStatisticsWidget__(QVBoxLayout(), data_vector)
-
-        self.setWidget(self.dockComposite)
-        parent.addDockWidget(Qt.RightDockWidgetArea, self)
-
-    def __createStatisticsWidget__(self, _layout, _data_vector):
-        self.statisticsWidget = TachogramStatisticsWidget(self.dockComposite,
-                                                          layout=_layout)
-        statistics = calculate_tachogram_statistics(signal=_data_vector.signal,
-                                            annotation=_data_vector.annotation)
-        self.statisticsWidget.setTachogramStatistics(statistics)
---
+++ PyGUI/src/pygui/qt/plots/tachogram_plot_statistics_dock_widget.py
@@ -0,0 +1,103 @@
+'''
+Created on 04-04-2013
+
+@author: jurek
+'''
+from pycore.special import ImportErrorMessage
+try:
+    from PyQt4.QtCore import *  # @UnusedWildImport
+    from PyQt4.QtGui import *  # @UnusedWildImport
+    from pycore.misc import Params
+    from pymath.datasources import FileDataSource
+    from pymath.statistics.tachogram_statistics import calculate_tachogram_statistics  # @IgnorePep8
+    from pygui.qt.utils.widgets import TableViewCommon
+    from pygui.qt.utils.widgets import CompositeCommon
+    from pygui.qt.utils.widgets import DockWidgetCommon
+    from pygui.qt.plots.tachogram_plot_plot import STATISTIC_MIME_ID
+    from pygui.qt.plots.tachogram_plot_plot import STATISTIC_CLASS_NAME_ID
+    from pygui.qt.utils.dnd import CopyDragger
+except ImportError as error:
+    ImportErrorMessage(error, __name__)
+
+
+class TachogramPlotStatisticsDockWidget(DockWidgetCommon):
+    """
+    a dock widget for tachogram plot statistics
+    """
+    def __init__(self, parent, **params):
+        self.params = Params(**params)
+        super(TachogramPlotStatisticsDockWidget, self).__init__(parent,
+                        title=params.get('title', 'Tachogram plot statistics'),
+                        **params)
+        file_data_source_params = self.params.file_specification._asdict()
+        file_data_source = FileDataSource(**file_data_source_params)
+        data_vector = file_data_source.getData()
+
+        self.setObjectName("TachogramPlotStatisticsDockWidget")
+        self.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea |
+                             Qt.TopDockWidgetArea | Qt.BottomDockWidgetArea)
+        layout = QVBoxLayout()
+        layout.setMargin(0)  # no margin for internal layout
+        self.dockComposite = CompositeCommon(self, layout=layout,
+                                        not_add_widget_to_parent_layout=True)
+        self.__createStatisticsWidget__(QVBoxLayout(), data_vector)
+
+        self.setWidget(self.dockComposite)
+        parent.addDockWidget(Qt.RightDockWidgetArea, self)
+
+    def __createStatisticsWidget__(self, _layout, _data_vector):
+        self.statisticsWidget = TachogramStatisticsWidget(self.dockComposite,
+                                                          layout=_layout)
+        statistics = calculate_tachogram_statistics(signal=_data_vector.signal,
+                                            annotation=_data_vector.annotation)
+        self.statisticsWidget.setTachogramStatistics(statistics)
+
+
+class TachogramStatisticsWidget(TableViewCommon):
+    """
+    a widget to display basic tachogram's statistics
+    """
+    def __init__(self, parent, **params):
+        TableViewCommon.__init__(self, parent, **params)
+        self.__dragger__ = CopyDragger(self, STATISTIC_MIME_ID, drag_only=True)
+        self.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.setColumnHidden(0, True)  # "classname" columns is hidden
+        self.__createModel__()
+
+    def __createModel__(self):
+        model = TachogramStatisticsModel(self)
+        labels = QStringList(["class_name", "Statistic", "Value"])
+        model.setHorizontalHeaderLabels(labels)
+        self.setModel(model)
+
+    def setTachogramStatistics(self, _statistics):
+        model = self.model()
+        model.removeRows(0, model.rowCount())
+        values = _statistics[0]
+        descriptions = _statistics[1]
+        self.class_names = sorted([name for name in values])
+        for name in sorted([name for name in self.class_names]):
+            model.appendRow([QStandardItem(str(name)),
+                             QStandardItem(str(descriptions[name])),
+                             QStandardItem(str(values[name]))])
+
+    def startDrag(self, dropActions):
+        row = self.model().itemFromIndex(self.currentIndex()).row()
+        self.__dragger__.clear()
+        self.__dragger__.dragObject(STATISTIC_CLASS_NAME_ID,
+                                    self.class_names[row])
+        self.__dragger__.startDrag()
+
+
+class TachogramStatisticsModel(QStandardItemModel):
+    def __init__(self, parent):
+        QStandardItemModel.__init__(self, parent=parent)
+
+    def data(self, _modelIndex, _role):
+        #the first column (with index 0) is a name of statistic
+        if _modelIndex.column() == 1 and _role == Qt.TextAlignmentRole:
+            return Qt.AlignRight
+        else:
+            return super(TachogramStatisticsModel, self).data(_modelIndex,
+                                                              _role)
