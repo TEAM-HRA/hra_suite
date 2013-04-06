7c0aec4fcc80f2778e9be789dcd006157548a076
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-04-06 19:49:54 +0000

    extract and change name of the class pygui.qt.plots.tachogram_plot_tab_widget.DatasourceListWidget into pygui.qt.plots.tachogram_plot_datasource_list_widget.TachogramPlotDatasourceListWidget
    git-svn-id: https://jurek-pc3/repositories/doctorate@560 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/plots/tachogram_plot_tab_widget.py
+++ PyGUI/src/pygui/qt/plots/tachogram_plot_tab_widget.py
@@ -9,20 +9,10 @@ try:
     from PyQt4.QtGui import *  # @UnusedWildImport
     from pycore.misc import Params
     from pygui.qt.utils.windows import AreYouSureWindow
-    from pygui.qt.utils.windows import showFilesPreviewDialog
-    from pygui.qt.utils.widgets import PushButtonCommon
-    from pygui.qt.utils.widgets import WidgetCommon
-    from pygui.qt.utils.widgets import ListWidgetCommon
-    from pygui.qt.utils.widgets import CheckBoxCommon
     from pygui.qt.utils.signals import TAB_WIDGET_ADDED_SIGNAL
     from pygui.qt.utils.signals import SignalDispatcher
-    from pygui.qt.utils.widgets import ListWidgetItemCommon
     from pygui.qt.custom_widgets.splitter import SplitterWidget
-    from pygui.qt.custom_widgets.toolbars import OperationalToolBarWidget
-    from pygui.qt.custom_widgets.toolbars import ToolBarManager
-    from pygui.qt.custom_widgets.toolbars import CheckUncheckToolBarWidget
     from pygui.qt.custom_widgets.tabwidget import TabWidgetItemCommon
-    from pygui.qt.custom_widgets.progress_bar import ProgressDialogManager
     from pygui.qt.utils.widgets import maximize_widget
     from pygui.qt.utils.widgets import restore_widget
     from pygui.qt.utils.signals import ENABLEMEND_SIGNAL
@@ -30,6 +20,7 @@ try:
     from pygui.qt.plots.plots_signals import MAXIMIZE_TACHOGRAM_PLOT_SIGNAL
     from pygui.qt.plots.plots_signals import RESTORE_TACHOGRAM_PLOT_SIGNAL
     from pygui.qt.plots.tachogram_plot_manager import TachogramPlotManager
+    from pygui.qt.plots.tachogram_plot_datasource_list_widget import TachogramPlotDatasourceListWidget # @IgnorePep8
 except ImportError as error:
     ImportErrorMessage(error, __name__)
 
@@ -59,7 +50,8 @@ class TachogramPlotTabWidget(TabWidgetItemCommon):
 
     def __createDatasourceListWidget__(self):
         self.__datasourceListWidget__ = \
-            DatasourceListWidget(self.__splitter__, self.params.model,
+            TachogramPlotDatasourceListWidget(self.__splitter__,
+                                              self.params.model,
                 add_tachogram_plot_handler=self.__addTachogramPlot__,
                 close_tachogram_plot_handler=self.closeTab,
                 close_tachograms_handler=self.__closeTachogramsHandler__)
@@ -116,126 +108,3 @@ class TachogramPlotTabWidget(TabWidgetItemCommon):
 
     def __restoreTachogramPlot__(self):
         restore_widget(self.__tachogramsManager__)
-
-
-class DatasourceListWidget(WidgetCommon):
-    def __init__(self, parent, model, **params):
-        super(DatasourceListWidget, self).__init__(parent,
-                                                    add_widget_to_parent=True,
-                                                    layout=QVBoxLayout())
-        self.params = Params(**params)
-        toolbars = ToolBarManager(self, CheckUncheckToolBarWidget,
-                OperationalToolBarWidget,
-                toolbar_close_handler=self.params.close_tachogram_plot_handler)
-        self.layout().addWidget(toolbars)
-
-        self.__showTachogramsButton__ = PushButtonCommon(self,
-                    i18n="poincare.plot.show.tachograms.button",
-                    i18n_def="Show tachograms",
-                    enabled=False,
-                    clicked_handler=self.__showTachogramsHandler__,
-                    enabled_precheck_handler=self.__enabledPrecheckHandler__)
-
-        self.__allowTachogramsDuplicationButton__ = CheckBoxCommon(self,
-                    i18n="poincare.plot.allow.tachograms.duplications.button",
-                    i18n_def="Allow tachograms duplication",
-                    enabled=False,
-                    enabled_precheck_handler=self.__enabledPrecheckHandler__)
-
-        self.__closeAllTachogramsButton__ = PushButtonCommon(self,
-                    i18n="poincare.plot.close.all.tachograms.button",
-                    i18n_def="Close all tachograms",
-                    enabled=False,
-                    clicked_handler=self.__closeTachogramsHandler__)
-
-        self.__datasourceList__ = \
-            ListWidgetCommon(self,
-                list_item_clicked_handler=self.__datasourceItemClickedHandler__, # @IgnorePep8
-                list_item_double_clicked_handler=self.__datasourceDoubleItemClickedHandler__, # @IgnorePep8
-                selectionMode=QAbstractItemView.MultiSelection,
-                selectionBehavior=QAbstractItemView.SelectRows)
-        if len(model) > 0:
-            for row in range(len(model)):
-                fileSpecification = model[row]
-                ListWidgetItemCommon(self.__datasourceList__,
-                                     text=fileSpecification.filename,
-                                     data=fileSpecification)
-        else:
-            ListWidgetItemCommon(self.__datasourceList__,
-                                 text='model not specified or incorrect type')
-
-        self.__filesPreviewButton__ = PushButtonCommon(self,
-                    i18n="poincare.plot.files.preview.button",
-                    i18n_def="Files preview",
-                    enabled=False,
-                    clicked_handler=self.__filesPreviewHandler__,
-                    enabled_precheck_handler=self.__enabledPrecheckHandler__)
-
-    def __datasourceItemClickedHandler__(self, listItem):
-        self.emit(ENABLEMEND_SIGNAL, listItem.isSelected())
-
-    def __showTachogramsHandler__(self):
-        self.__showTachograms__(self.__getFilesSpecifications__(selected=True))
-
-    def __filesPreviewHandler__(self):
-        showFilesPreviewDialog(self.__getFilesSpecifications__(selected=True))
-
-    def __datasourceDoubleItemClickedHandler__(self, listItem):
-        self.__showTachograms__(self.__getFilesSpecifications__([listItem]))
-
-    def __showTachograms__(self, files_specifications):
-        progressManager = ProgressDialogManager(self,
-                                        label_text="Create tachograms",
-                                        max_value=len(files_specifications))
-        firstFocus = True
-        checked = self.__allowTachogramsDuplicationButton__.isChecked()
-        with progressManager as progress:
-            for idx in range(progress.maximum()):
-                if (progress.wasCanceled()):
-                    break
-                progress.increaseCounter()
-                tachogram_plot = self.params.add_tachogram_plot_handler(
-                                            files_specifications[idx],
-                                            checked, firstFocus)
-                if firstFocus and tachogram_plot:
-                    firstFocus = False
-
-        self.__datasourceList__.clearSelection()
-
-    def __enabledPrecheckHandler__(self, widget):
-        """
-        only interested widgets return bool value others return none value
-        """
-        if widget in (self.__showTachogramsButton__,
-                      self.__allowTachogramsDuplicationButton__,
-                      self.__filesPreviewButton__):
-            return len(self.__datasourceList__.selectedIndexes()) > 0
-
-    def __closeTachogramsHandler__(self):
-        if self.params.close_tachograms_handler:
-            if self.params.close_tachograms_handler():
-                self.__closeAllTachogramsButton__.setEnabled(False)
-        self.toolbar_uncheck_handler()
-
-    def toolbar_uncheck_handler(self):
-        self.__datasourceList__.clearSelection()
-        self.emit(ENABLEMEND_SIGNAL, False)
-
-    def toolbar_check_handler(self):
-        self.__datasourceList__.selectAll()
-        self.emit(ENABLEMEND_SIGNAL, True)
-
-    def toolbar_maximum_handler(self):
-        maximize_widget(self)
-
-    def toolbar_restore_handler(self):
-        restore_widget(self)
-
-    def enabledCloseAllTachogramsButton(self, enabled):
-        self.__closeAllTachogramsButton__.setEnabled(enabled)
-
-    def __getFilesSpecifications__(self, list_items=None, selected=False):
-        if selected:  # get files specifications from selected items
-            list_items = self.__datasourceList__.selectedItems()
-        #acquired from data buffer of list items file specification objects
-        return [list_item.getData() for list_item in list_items]
