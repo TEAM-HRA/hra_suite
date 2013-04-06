b2dda675c5c3f43d7ee8ee12f2e5d795bc8f9da0
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-04-06 19:06:18 +0000

    move and change name of the class pygui.qt.plots.tachogram_plot_plot.TachogramPlotPlot into module pygui.qt.plots.tachogram_plot_window and name TachogramPlot
    git-svn-id: https://jurek-pc3/repositories/doctorate@558 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/plots/tachogram_plot_window.py
+++ PyGUI/src/pygui/qt/plots/tachogram_plot_window.py
@@ -9,15 +9,19 @@ try:
     from PyQt4.QtCore import *  # @UnusedWildImport
     from pycore.misc import Params
     from pycore.introspection import get_child_of_type
+    from pycore.units import get_unit_by_class_name
+    from pymath.datasources import FileDataSource
     from pygui.qt.utils.widgets import MainWindowCommon
     from pygui.qt.custom_widgets.toolbars import OperationalToolBarWidget
-    from pygui.qt.plots.tachogram_plot_plot import TachogramPlotPlot
+    from pygui.qt.utils.widgets import CompositeCommon
     from pygui.qt.utils.signals import SignalDispatcher
     from pygui.qt.plots.plots_signals import CLOSE_TACHOGRAM_PLOT_SIGNAL
     from pygui.qt.plots.plots_signals import MAXIMIZE_TACHOGRAM_PLOT_SIGNAL
     from pygui.qt.plots.plots_signals import RESTORE_TACHOGRAM_PLOT_SIGNAL
     from pygui.qt.plots.tachogram_plot_settings_dock_widget import TachogramPlotSettingsDockWidget # @IgnorePep8
     from pygui.qt.plots.tachogram_plot_statistics_dock_widget import TachogramPlotStatisticsDockWidget  # @IgnorePep8
+    from pygui.qt.plots.tachogram_plot_canvas import TachogramPlotCanvas
+    from pygui.qt.plots.tachogram_plot_navigator_toolbar import TachogramPlotNavigationToolbar  # @IgnorePep8    
 except ImportError as error:
     ImportErrorMessage(error, __name__)
 
@@ -29,7 +33,7 @@ class TachogramPlotWindow(MainWindowCommon):
 
         self.addToolBar(OperationalToolBarWidget(self))
 
-        self.tachogramPlot = TachogramPlotPlot(self,
+        self.tachogramPlot = TachogramPlot(self,
             file_specification=self.params.file_specification,
             show_tachogram_plot_settings_handler=self.__show_tachogram_plot_settings_handler__,   # @IgnorePep8
             show_tachogram_plot_statistics_handler=self.__show_tachogram_plot_statistics_handler__)  # @IgnorePep8
@@ -68,3 +72,32 @@ class TachogramPlotWindow(MainWindowCommon):
 #                    i18n_def="STATUS",
 #                    add_widget_to_parent=True)
 #
+
+
+class TachogramPlot(CompositeCommon):
+    """
+    this class represents core of the tachogram plot that is a plot itself
+    """
+    def __init__(self, parent, **params):
+        super(TachogramPlot, self).__init__(parent,
+                                        not_add_widget_to_parent_layout=True)
+        self.params = Params(**params)
+        file_data_source_params = self.params.file_specification._asdict()
+        self.signal_unit = get_unit_by_class_name(
+                        file_data_source_params.get('signal_unit_class_name'))
+        file_data_source = FileDataSource(**file_data_source_params)
+        data = file_data_source.getData()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        self.canvas = TachogramPlotCanvas(self, signal=data.signal,
+                                          annotation=data.annotation,
+                                          signal_unit=data.signal_unit)
+        layout.addWidget(self.canvas)
+        self.navigation_toolbar = TachogramPlotNavigationToolbar(self.canvas,
+                            self,
+                            show_tachogram_plot_settings_handler=self.params.show_tachogram_plot_settings_handler, # @IgnorePep8
+                            show_tachogram_plot_statistics_handler=self.params.show_tachogram_plot_statistics_handler) # @IgnorePep8
+        layout.addWidget(self.navigation_toolbar)
+
+    def changeXUnit(self, _unit):
+        self.canvas.changeXUnit(_unit)
