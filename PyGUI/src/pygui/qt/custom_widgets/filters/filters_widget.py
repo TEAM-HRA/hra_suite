c8c35b03ca92ed1289308194eb57e8df659bd404
jurek <jurek@13b80749-e00b-5047-ae1c-0d8bb577e5e7>
2013-04-13 16:57:14 +0000

    changes in a module pygui.qt.custom_widgets.filters.filters_widget put all filters into a group box widget; change a name of a button 'Restore filters' into 'Back to unfiltered data', this button is activated only when any filter is activated; add a listener class __FilterActivatedDataVectorListener__ to listen filter activation
    git-svn-id: https://jurek-pc3/repositories/doctorate@581 13b80749-e00b-5047-ae1c-0d8bb577e5e7

--- PyGUI/src/pygui/qt/custom_widgets/filters/filters_widget.py
+++ PyGUI/src/pygui/qt/custom_widgets/filters/filters_widget.py
@@ -8,8 +8,10 @@ try:
     from PyQt4.QtGui import *  # @UnusedWildImport
     from PyQt4.QtCore import *  # @UnusedWildImport
     from pycore.collections_utils import get_or_put
+    from pymath.datasources import DataVectorListener
     from pygui.qt.utils.widgets import CompositeCommon
     from pygui.qt.utils.widgets import PushButtonCommon
+    from pygui.qt.utils.widgets import GroupBoxCommon
     from pygui.qt.custom_widgets.filters.annotation_filter_widget import AnnotationFilterWidget # @IgnorePep8
     from pygui.qt.custom_widgets.filters.square_filter_widget import SquareFilterWidget # @IgnorePep8
 except ImportError as error:
@@ -25,16 +27,39 @@ class FiltersWidget(CompositeCommon):
         get_or_put(params, 'layout', QVBoxLayout())
         super(FiltersWidget, self).__init__(parent, **params)
         self.data_accessor = self.params.data_accessor  # alias
-        #self.addItem("- Filters -")
-        self.__annotation_filter__ = AnnotationFilterWidget(self,
+        self.data_accessor.addListener(self, __FilterActivatedDataVectorListener__(self)) # @IgnorePep8        
+
+        filtersGroup = GroupBoxCommon(self, layout=QVBoxLayout(),
+                                      i18n_def='Filters')
+        self.__annotation_filter__ = AnnotationFilterWidget(filtersGroup,
                                     data_accessor=self.data_accessor)
-        self.__square_filter__ = SquareFilterWidget(self,
+        self.__square_filter__ = SquareFilterWidget(filtersGroup,
                                     data_accessor=self.data_accessor)
 
-        PushButtonCommon(self, i18n_def='Restore filters',
-                        clicked_handler=self.__restore_handler__)
+        self.__restore_button__ = PushButtonCommon(filtersGroup,
+                                     i18n_def='Back to unfiltered data',
+                                     clicked_handler=self.__restore_handler__,
+                                     enabled=False)
 
     def __restore_handler__(self):
         self.data_accessor.restore()
         self.__annotation_filter__.reset()
         self.__square_filter__.reset()
+        self.__restore_button__.setEnabled(False)
+
+    def enableRestoreButton(self):
+        self.__restore_button__.setEnabled(True)
+
+
+class __FilterActivatedDataVectorListener__(DataVectorListener):
+    """
+    this class is run when any filter is activated
+    """
+    def __init__(self, _filter_widget):
+        self.__filter_widget__ = _filter_widget
+
+    def changeSignal(self, _signal):
+        self.__filter_widget__.enableRestoreButton()
+
+    def changeAnnotation(self, _annotation):
+        self.__filter_widget__.enableRestoreButton()
