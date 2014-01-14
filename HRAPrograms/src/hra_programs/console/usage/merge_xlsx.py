c8e59e7292987f5230b4d5c96e8616913852eb62
jurek <jeel@epoczta.pl>
2014-01-14 18:06:27 +0100

    add in merge_xlsx.py program new parameters -index_file_xls  (--index_file_xls) 	xls index file -index_sheet_name (--index_sheet_name) 	index sheet name -index_code_name (--index_code_name) 	index code name used to specify parameters of a separated file for indexes data connecting rows in source_xls file with names of source csv files

--- HRAPrograms/src/hra_programs/console/usage/merge_xlsx.py
+++ HRAPrograms/src/hra_programs/console/usage/merge_xlsx.py
@@ -13,6 +13,7 @@ from hra_core.io_utils import get_filename
 from hra_core.misc import extract_number
 
 # example of eclipse parameters:
+################################
 #-source_xls '/home/jurek/tmp/klinika0.xlsx'
 #-output_xls '/home/jurek/tmp/klinika_out.xlsx'
 #-sources_csv_files_mask '/home/jurek/volumes/doctoral/monitor_do_impedancji_niccomo_wyniki/CO_team/*.res_out',  # @IgnorePep8 one line
@@ -21,19 +22,34 @@ from hra_core.misc import extract_number
 #                        '/home/jurek/volumes/doctoral/monitor_do_impedancji_niccomo_wyniki/SV_team/*.res_out',  # @IgnorePep8   one line
 #                        '/home/jurek/volumes/doctoral/monitor_do_impedancji_niccomo_wyniki/SVR_team/*.res_out'  # @IgnorePep8   one line
 #-separator ';'
+#-index_file_xls '/home/jurek/tmp/indexes.xlsx'
+#-index_code_name kodlast7
+#-index_sheet_name Arkusz1
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-source_xls", "--source_xls", help="source xls file")
 parser.add_argument("-output_xls", "--output_xls", help="output xls file")
+parser.add_argument("-index_file_xls", "--index_file_xls",
+                    help="xls index file")
+parser.add_argument("-index_sheet_name", "--index_sheet_name",
+                    help="index sheet name")
+parser.add_argument("-index_code_name", "--index_code_name",
+                    help="index code name")
 parser.add_argument("-sources_csv_files_mask", "--sources_csv_files_mask",
                     help="source csv files list mask")
 parser.add_argument("-separator", "--separator", help="separator")
 __args = parser.parse_args()
 
 
-def merge_xls(output_xls, source_csv_files_mask, separator):
+def merge_xls(output_xls, source_csv_files_mask, separator,
+              index_file_xls, index_sheet_name, index_code_name):
     print('***************************************************')
     print('csv source files: ' + source_csv_files_mask)
+
+    index_xls = load_workbook(index_file_xls)
+    index_sheet = index_xls.get_sheet_by_name(index_sheet_name)
+    index_cells = zip(index_sheet.columns[0], index_sheet.columns[1])
+
     xls = None
     merged_sheet = None
     for file_nr, _file in enumerate(glob.glob(source_csv_files_mask)):
@@ -44,8 +60,8 @@ def merge_xls(output_xls, source_csv_files_mask, separator):
         if xls == None:
             xls = load_workbook(output_xls)
             merged_sheet = xls.get_sheet_by_name('merged')
-            lp_cells = merged_sheet.columns[0]
             headers_cells = merged_sheet.rows[0]
+            code_cells = merged_sheet.columns[2]
 
         (headers_line, values_line) = get_first_lines(_file, count=2)
         filename = get_filename(_file)
@@ -56,10 +72,14 @@ def merge_xls(output_xls, source_csv_files_mask, separator):
 
         file_number = extract_number(file_ident)
         ident_row = None
-        for cell in lp_cells:
-            if file_number == cell.value:
-                ident_row = cell.row - 1
+        for (file_index_cell, code_index_cell) in index_cells:
+            if not ident_row == None:
                 break
+            if file_number == file_index_cell.value:
+                for code_cell in code_cells:
+                    if code_cell.value == code_index_cell.value:
+                        ident_row = code_cell.row - 1
+                        break
 
         #print("ident_row: " + str(ident_row))
         #add header
@@ -104,6 +124,8 @@ copyfile(__args.source_xls, __args.output_xls)
 print(".. done")
 
 for source_csv_files_mask in __args.sources_csv_files_mask.split(','):
-    merge_xls(__args.output_xls, source_csv_files_mask, __args.separator)
+    merge_xls(__args.output_xls, source_csv_files_mask, __args.separator,
+              __args.index_file_xls, __args.index_sheet_name,
+              __args.index_code_name)
 
 print('The End')
