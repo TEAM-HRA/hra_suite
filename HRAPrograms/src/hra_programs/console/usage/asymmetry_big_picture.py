bb931b606bc07d82a4672d745caf60860a0da5c7
jurek <jeel@epoczta.pl>
2014-07-27 22:57:41 +0200

    1) change layout of the plot: ekg image with first tachogram plot in a one row, two plots with marked data windows in the second row 2) use FontProperties class to set up font's properties suitable elements of the plot 3) bold plot's ticks

--- HRAPrograms/src/hra_programs/console/usage/asymmetry_big_picture.py
+++ HRAPrograms/src/hra_programs/console/usage/asymmetry_big_picture.py
@@ -41,6 +41,21 @@ font_0 = FontProperties('DejaVu Sans')
 from matplotlib.gridspec import GridSpec
 import matplotlib.patches as mpatches
 
+
+def create_empty_plot(gs):
+    # empty plot
+    empty_plot = plt.subplot(gs)
+    empty_plot.grid(False)
+    empty_plot.set_frame_on(False)
+    empty_plot.title.set_visible(False)
+    empty_plot.xaxis.set_major_locator(plt.NullLocator())
+    empty_plot.yaxis.set_major_locator(plt.NullLocator())
+    for tl in empty_plot.get_xticklabels():
+        tl.set_visible(False)
+    for tl in empty_plot.get_yticklabels():
+        tl.set_visible(False)
+
+
 def bold_ticks_labels(ax, fontsize=11):
     # We need to draw the canvas, otherwise the labels won't be positioned and
     # won't have values yet.
@@ -55,73 +70,58 @@ def bold_ticks_labels(ax, fontsize=11):
     ax.set_xticklabels(x_labels, fontproperties=font_1)
     ax.set_yticklabels(y_labels, fontproperties=font_1)
 
-
-
-def make_ticklabels_invisible(fig):
-    return
-    for i, ax in enumerate(fig.axes):
-        #ax.text(0.5, 0.5, "ax%d" % (i+1), va="center", ha="center")
-        for tl in ax.get_xticklabels() + ax.get_yticklabels():
-            tl.set_visible(False)
-
-
-
-# demo 3 : gridspec with subplotpars set.
+# def make_ticklabels_invisible(fig):
+#     return
+#     for i, ax in enumerate(fig.axes):
+#         #ax.text(0.5, 0.5, "ax%d" % (i+1), va="center", ha="center")
+#         for tl in ax.get_xticklabels() + ax.get_yticklabels():
+#             tl.set_visible(False)
 
 f = plt.figure()
 
 max_idx = 23
 
-def can_show(show_rows, idx):
-    return True if idx < len(show_rows) and show_rows[idx] else False
+gs0 = GridSpec(3, 1, height_ratios=[3.0, 0.5, 6.5])
+gs0.update(left=0.04, right=0.99, wspace=0.01, hspace=0.0, bottom=0.05, top=0.98)
 
+gs00 = gridspec.GridSpecFromSubplotSpec(1, 4, subplot_spec=gs0[0],
+                                        width_ratios=[1.5, 0.3, 0.35, 7.85],
+                                        wspace=0.0, hspace=0.0)
 
-show_rows = [True, True, True, True, True]
-#show_rows = [False, True, False, False, False]
-all_ratios = [3, 0.3, 3, 0.5, 4]
-height_ratios = [all_ratios[idx] for idx, s in enumerate(show_rows) if s == True]
-num_rows = len([s for s in show_rows if s == True])
-num_cols = 2
+##############################################
+##
+## first subgrid
+##
+##############################################
 
-row_number = 0
+# EKG Axis
+ax_ekg = plt.subplot(gs00[0, 0])
+ekg_img = mpimg.imread('/home/jurek/git_repos/doctorate/Doktorat/graph/ekg.png')
+ax_ekg.imshow(ekg_img) #, aspect='auto') #, origin='lower')
+ax_ekg.set_axis_off()
+ax_ekg.set_frame_on(False)
+ax_ekg.autoscale_view(tight=True)
 
-#plt.suptitle("GirdSpec w/ different subplotpars")
-#plt.suptitle("Schemat nowego dynamicznego podejscia w analizie asymetrii rytmu serca wykorzystujacego okno danych.")
+alignment = {'horizontalalignment':'left', 'verticalalignment':'center'}
+text_fontsize = 20
+font_1 = font_0.copy()
+font_1.set_size(text_fontsize)
+font_1.set_weight('bold')
+ax_ekg.text(0.1, 0.1, u"EKG", fontproperties=font_1, **alignment)
 
-gs1 = GridSpec(num_rows, num_cols, height_ratios=height_ratios) #[3, 0.3, 3, 0.5, 4])
-#gs1.update(left=0.05, right=0.48, wspace=0.05)
-gs1.update(left=0.04, right=0.99, wspace=0.1, hspace=0.2, bottom=0.04, top=0.98)
+####################################
+ax_arrow_left = plt.subplot(gs00[0, 1])
+ax_arrow_left.set_axis_off()
+ax_arrow_left.set_frame_on(False)
 
-if can_show(show_rows, 0):
-    ax_ekg = plt.subplot(gs1[row_number, :])
-    ekg_img = mpimg.imread('/home/jurek/git_repos/doctorate/Doktorat/graph/ekg.png')
-    ax_ekg.imshow(ekg_img) #, aspect='auto') #, origin='lower')
-    ax_ekg.set_axis_off()
-    ax_ekg.set_frame_on(False)
-    ax_ekg.autoscale_view(tight=True)
+arrow_left = mpatches.Arrow(0.0, 0.5, 0.9, 0.0, width=0.25, color="black", lw=4) #, label="Dane z EKG")
+arrow_left.set_fill(False)
+ax_arrow_left.add_patch(arrow_left)
 
-    alignment = {'horizontalalignment':'left', 'verticalalignment':'center'}
-    text_fontsize = 20
-    font_1 = font_0.copy()
-    font_1.set_size(text_fontsize)
-    font_1.set_weight('bold')
-    ax_ekg.text(0.1, 0.1, u"EKG", fontproperties=font_1, **alignment)
-    row_number = row_number + 1
+#empty plot
+create_empty_plot(gs00[0,2])
 
-if can_show(show_rows, 1):
-    ax_arrow_down = plt.subplot(gs1[row_number, :])
-    patches = []
-    arrow_down = mpatches.Arrow(0.5, 1, -0, -0.9, width=0.05, color="grey", label="Dane z EKG")
-    arrow_down.set_fill(True)
-    patches.append(arrow_down)
-    collection = PatchCollection(patches,  alpha=0.3) #cmap=plt.cm.hsv,)
-    ax_arrow_down.set_axis_off()
-    ax_arrow_down.set_frame_on(False)
-    #ax_arrow_down.autoscale_view(tight=True)
-    ax_arrow_down.add_patch(arrow_down)
-    row_number = row_number + 1
-
-
+###################################
 file_rr = "/home/jurek/volumes/doctoral/dane/long_corrected/jakubas_RR_P.rea"
 usecols = (1, 2,)
 des1, annotation = np.loadtxt(file_rr, usecols=usecols, skiprows=1, unpack=True)
@@ -137,176 +137,177 @@ des1_min = np.min(des1)
 max_timing = np.max(timing)
 
 
-if can_show(show_rows, 2):
-    ax_tachogram = plt.subplot(gs1[row_number, :])
-    ax_tachogram.set_color_cycle(['blue'])
-    ax_tachogram.plot(timing, des1)
-    ax_tachogram.ticklabel_format(style='sci', axis='x', scilimits=(0, max_timing))
-    ax_tachogram.xaxis.set_ticks(np.arange(0, int(max_timing) + 1, 1))
-    ax_tachogram.set_xlim(0, max_timing)
-    leg = ax_tachogram.legend([u"$\mathbf{%s}$" % ("RR")], loc='upper left')
-    plt.setp(leg.get_texts(), fontsize='large')
-    plt.setp(leg.get_texts(), fontweight='bold')
+#################################
+ax_tachogram = plt.subplot(gs00[0, 3])
+ax_tachogram.set_color_cycle(['blue'])
+ax_tachogram.plot(timing, des1)
+ax_tachogram.ticklabel_format(style='sci', axis='x', scilimits=(0, max_timing))
+ax_tachogram.xaxis.set_ticks(np.arange(0, int(max_timing) + 1, 1))
+ax_tachogram.set_xlim(0, max_timing)
+leg = ax_tachogram.legend([u"$\mathbf{%s}$" % ("RR")], loc='upper left')
+plt.setp(leg.get_texts(), fontsize='large')
+plt.setp(leg.get_texts(), fontweight='bold')
 
-    font_1 = font_0.copy()
-    font_1.set_size('11')
-    font_1.set_weight('bold')
-    ax_tachogram.set_xlabel(u"Czas [godziny]", fontproperties=font_1)
-    ax_tachogram.set_ylabel(u"Wartość [ms]", fontproperties=font_1)
-    #ax_tachogram.set_text(0.8, 0.8, 'Tachogram')
+font_1 = font_0.copy()
+font_1.set_size('11')
+font_1.set_weight('bold')
+ax_tachogram.set_xlabel(u"Czas [godziny]", fontproperties=font_1)
+ax_tachogram.set_ylabel(u"Wartość [ms]", fontproperties=font_1)
+#ax_tachogram.set_text(0.8, 0.8, 'Tachogram')
 
-    tachogram_label_pos = 20
-    font_1 = font_0.copy()
-    font_1.set_size('18')
-    font_1.set_weight('bold')
-    ax_tachogram.text(max_idx - max_idx / 11.8, des1_max - des1_max / 70,
-                      u"Tachogram", fontproperties=font_1)
-    bold_ticks_labels(ax_tachogram)
+tachogram_label_pos = 20
+font_1 = font_0.copy()
+font_1.set_size('18')
+font_1.set_weight('bold')
+ax_tachogram.text(max_idx - max_idx / 11.8, des1_max - des1_max / 70,
+                  u"Tachogram", fontproperties=font_1)
+bold_ticks_labels(ax_tachogram)
 
-    #print('Title: ' + str(ax_tachogram.get_title()))
-    #ax_tachogram.set_title(None)
-    row_number = row_number + 1
+##############################################
+##
+## second subgrid
+##
+##############################################
 
-
-#arrow = mpatches.Arrow(grid[5, 0]-0.05, grid[5, 1]-0.05, 0.1, 0.1, width=0.1)
-
-if can_show(show_rows, 3):
-
-    text_fontsize = 20
-    alignment = {'horizontalalignment':'center', 'verticalalignment':'center'}
-    font_1 = font_0.copy()
-    font_1.set_size(text_fontsize)
-    font_1.set_weight('bold')
-
-    arrow_left = mpatches.Arrow(0.9, 1.1, -0.05, -1, width=0.15, color="black", label="Analiza")
-    arrow_left.set_fill(True)
-    #patches = []
-    #patches.append(arrow_left)
-    #collection = PatchCollection(patches,  alpha=0.3) #cmap=plt.cm.hsv,)
-    #colors = np.linspace(0, 1, len(patches))
-    #collection.set_array(np.array(colors))
-    ax_arrow_left = plt.subplot(gs1[row_number,0])
-    ax_arrow_left.text(0.5, 0.5, u"Standardowa analiza HRA całego nagrania" ,
-                       fontproperties=font_1, **alignment)
-    ax_arrow_left.set_axis_off()
-    ax_arrow_left.set_frame_on(False)
-    #ax_arrow_left.autoscale_view(tight=True)
-    #ax_arrow_left.add_collection(collection)
-    ax_arrow_left.add_patch(arrow_left)
+gs_empty = gridspec.GridSpecFromSubplotSpec(1, 3, subplot_spec=gs0[1],
+                                            wspace=0.0, hspace=0.0)
+empty_arrow = plt.subplot(gs_empty[0, :])
+create_empty_plot(gs_empty[0, :])
 
 
-    arrow_right = mpatches.Arrow(0.1, 1.1, 0.05, -1, width=0.15, color="green")
-    arrow_right.set_fill(True)
-    ax_arrow_right = plt.subplot(gs1[row_number,1])
-    ax_arrow_right.text(0.5, 0.5, u"Nowa analiza HRA przy pomocy okna danych", color="green",
-                        fontproperties=font_1, **alignment)
-    ax_arrow_right.set_axis_off()
-    ax_arrow_right.set_frame_on(False)
-    #ax_arrow_right.autoscale_view(tight=True)
-    #ax_arrow_left.add_collection(collection)
-    ax_arrow_right.add_patch(arrow_right)
-    row_number = row_number + 1
+##############################################
+##
+## third subgrid
+##
+##############################################
 
-if can_show(show_rows, 4):
-    # LEFT TACHOGRAM START
-    ax_left_tachogram = plt.subplot(gs1[row_number,0])
-    ax_left_tachogram.set_color_cycle(['blue', 'red'])
-    ax_left_tachogram.plot(timing, des1)
-    ax_left_tachogram.ticklabel_format(style='sci', axis='x', scilimits=(0, max_timing))
-    ax_left_tachogram.xaxis.set_ticks(np.arange(0, int(max_timing)+1, 1))
-    ax_left_tachogram.set_xlim(0, max_timing)
+gs01 = gridspec.GridSpecFromSubplotSpec(2, 3, subplot_spec=gs0[2],
+                                        height_ratios=[1, 9],
+                                        width_ratios=[4.825, 0.35, 4.825],
+                                        wspace=0.0, hspace=0.0)
 
-    font_1 = font_0.copy()
-    font_1.set_size('11')
-    font_1.set_weight('bold')
+#################################
 
-    ax_left_tachogram.set_xlabel(u"Czas [godziny]", fontproperties=font_1)
-    ax_left_tachogram.set_ylabel(u"Wartość [ms]", fontproperties=font_1)
+ax_arrow = plt.subplot(gs01[0, :])
+ax_arrow.set_axis_off()
+ax_arrow.set_frame_on(False)
 
-    font_1 = font_0.copy()
-    font_1.set_size('20')
-    font_1.set_weight('bold')
-    ax_left_tachogram.text(max_idx - 4, des1_max - des1_max / 70,
-                      u"Tachogram", fontproperties=font_1)
-    #ax_left_tachogram.set_title("Tachogram")
+text_fontsize = 20
+alignment = {'horizontalalignment':'center', 'verticalalignment':'center'}
+font_1 = font_0.copy()
+font_1.set_size(text_fontsize)
+font_1.set_weight('bold')
 
-    bold_ticks_labels(ax_left_tachogram)
+ax_arrow.text(0.25, 0.5, u"Standardowa analiza HRA całego nagrania" ,
+                   fontproperties=font_1, **alignment)
 
+arrow_left = mpatches.Arrow(0.5, 1.1, -0.05, -1, width=0.07, color="black", label="Analiza")
+arrow_left.set_fill(True)
+ax_arrow.add_patch(arrow_left)
+
+ax_arrow.text(0.75, 0.5, u"Nowa analiza HRA przy pomocy okna danych", color="green",
+                    fontproperties=font_1, **alignment)
+
+arrow_right = mpatches.Arrow(0.51, 1.1, 0.05, -1, width=0.07, color="green")
+arrow_right.set_fill(True)
+ax_arrow.add_patch(arrow_right)
+
+
+#################################
+# LEFT TACHOGRAM START
+ax_left_tachogram = plt.subplot(gs01[1, 0])
+ax_left_tachogram.set_color_cycle(['blue', 'red'])
+ax_left_tachogram.plot(timing, des1)
+ax_left_tachogram.ticklabel_format(style='sci', axis='x', scilimits=(0, max_timing))
+ax_left_tachogram.xaxis.set_ticks(np.arange(0, int(max_timing)+1, 1))
+ax_left_tachogram.set_xlim(0, max_timing)
+
+font_1 = font_0.copy()
+font_1.set_size('11')
+font_1.set_weight('bold')
+
+ax_left_tachogram.set_xlabel(u"Czas [godziny]", fontproperties=font_1)
+ax_left_tachogram.set_ylabel(u"Wartość [ms]", fontproperties=font_1)
+
+font_1 = font_0.copy()
+font_1.set_size('20')
+font_1.set_weight('bold')
+ax_left_tachogram.text(max_idx - 4, des1_max - des1_max / 70,
+                  u"Tachogram", fontproperties=font_1)
+#ax_left_tachogram.set_title("Tachogram")
+
+bold_ticks_labels(ax_left_tachogram)
+
+vertices = []
+codes = []
+codes = [Path.MOVETO] + [Path.LINETO] * 3 + [Path.CLOSEPOLY]
+vertices = [(0, des1_max - des1_max / 10),
+            (max_idx, des1_max - des1_max / 10),
+            (max_idx, des1_min + des1_max / 10),
+            (0, des1_min + des1_max / 10), (0, 0)]
+vertices = np.array(vertices, float)
+path = Path(vertices, codes)
+pathpatch = PathPatch(path, facecolor='None', edgecolor='red', zorder=3, lw=4)
+ax_left_tachogram.add_patch(pathpatch)
+leg  = ax_left_tachogram.legend([u"$\mathbf{%s}$" % ("RR"), u"Okno danych - 24 godziny"], loc='upper left')
+plt.setp(leg.get_texts(), fontsize='large')
+plt.setp(leg.get_texts(), fontweight='bold')
+# LEFT TACHOGRAM STOP
+
+#empty plot
+create_empty_plot(gs01[1, 1])
+
+# RIGHT TACHOGRAM START
+ax_right_tachogram = plt.subplot(gs01[1, 2])
+ax_right_tachogram.set_color_cycle(['blue', 'red'])
+ax_right_tachogram.plot(timing, des1)
+ax_right_tachogram.ticklabel_format(style='sci', axis='x', scilimits=(0, max_timing))
+ax_right_tachogram.xaxis.set_ticks(np.arange(0, int(max_timing)+1, 1))
+ax_right_tachogram.set_xlim(0, max_timing)
+
+font_1 = font_0.copy()
+font_1.set_size('11')
+font_1.set_weight('bold')
+ax_right_tachogram.set_xlabel(u"Czas [godziny]", fontproperties=font_1)
+ax_right_tachogram.set_ylabel(u"Wartość [ms]", fontproperties=font_1)
+
+font_1 = font_0.copy()
+font_1.set_size('20')
+font_1.set_weight('bold')
+ax_right_tachogram.text(max_idx - 4, des1_max - des1_max / 70,
+                  u"Tachogram", fontproperties=font_1)
+
+first_lw = 0.5
+lws = np.linspace(first_lw, 4, max_idx + 1 + first_lw)[::-1]
+
+
+for idx, lw in zip(range(max_idx - 1, -1, -1), lws):
     vertices = []
     codes = []
     codes = [Path.MOVETO] + [Path.LINETO] * 3 + [Path.CLOSEPOLY]
-    vertices = [(0, des1_max - des1_max / 10),
-                (max_idx, des1_max - des1_max / 10),
-                (max_idx, des1_min + des1_max / 10),
-                (0, des1_min + des1_max / 10), (0, 0)]
+    vertices = [(idx, des1_max - des1_max / 10),
+                (idx + 1, des1_max - des1_max / 10),
+                (idx + 1, des1_min + des1_max / 10),
+                (idx, des1_min + des1_max / 10), (0, 0)]
     vertices = np.array(vertices, float)
     path = Path(vertices, codes)
-    pathpatch = PathPatch(path, facecolor='None', edgecolor='red', zorder=3, lw=4)
-    ax_left_tachogram.add_patch(pathpatch)
-    leg  = ax_left_tachogram.legend([u"$\mathbf{%s}$" % ("RR"), u"Okno danych - 24 godziny"], loc='upper left')
-    plt.setp(leg.get_texts(), fontsize='large')
-    plt.setp(leg.get_texts(), fontweight='bold')
-    # LEFT TACHOGRAM STOP
+    pathpatch = PathPatch(path, facecolor='None', edgecolor='red', zorder=3, lw=lw)
+    pathpatch.set_fill(False)
+    ax_right_tachogram.add_patch(pathpatch)
 
-    # RIGHT TACHOGRAM START
-    ax_right_tachogram = plt.subplot(gs1[row_number,1])
-    ax_right_tachogram.set_color_cycle(['blue', 'red'])
-    ax_right_tachogram.plot(timing, des1)
-    ax_right_tachogram.ticklabel_format(style='sci', axis='x', scilimits=(0, max_timing))
-    ax_right_tachogram.xaxis.set_ticks(np.arange(0, int(max_timing)+1, 1))
-    ax_right_tachogram.set_xlim(0, max_timing)
+leg = ax_right_tachogram.legend([u"$\mathbf{%s}$" % ("RR"), "Okno danych - 5 minut"],
+                                 loc='upper left', numpoints=5)
+plt.setp(leg.get_texts(), fontsize='large')
+plt.setp(leg.get_texts(), fontweight='bold')
 
-    font_1 = font_0.copy()
-    font_1.set_size('11')
-    font_1.set_weight('bold')
-    ax_right_tachogram.set_xlabel(u"Czas [godziny]", fontproperties=font_1)
-    ax_right_tachogram.set_ylabel(u"Wartość [ms]", fontproperties=font_1)
+bold_ticks_labels(ax_right_tachogram)
 
-    font_1 = font_0.copy()
-    font_1.set_size('20')
-    font_1.set_weight('bold')
-    ax_right_tachogram.text(max_idx - 4, des1_max - des1_max / 70,
-                      u"Tachogram", fontproperties=font_1)
-
-    max_idx = 23
-    #ax_right_tachogram.legend(numpoints=10)
-
-    first_lw = 0.5
-    lws = np.linspace(first_lw, 4, max_idx + 1 + first_lw)[::-1]
-
-
-    for idx, lw in zip(range(max_idx - 1, -1, -1), lws):
-        vertices = []
-        codes = []
-        codes = [Path.MOVETO] + [Path.LINETO] * 3 + [Path.CLOSEPOLY]
-        vertices = [(idx, des1_max - des1_max / 10),
-                    (idx + 1, des1_max - des1_max / 10),
-                    (idx + 1, des1_min + des1_max / 10),
-                    (idx, des1_min + des1_max / 10), (0, 0)]
-        vertices = np.array(vertices, float)
-        path = Path(vertices, codes)
-        pathpatch = PathPatch(path, facecolor='None', edgecolor='red', zorder=3, lw=lw)
-        pathpatch.set_fill(False)
-        ax_right_tachogram.add_patch(pathpatch)
-
-    leg = ax_right_tachogram.legend([u"$\mathbf{%s}$" % ("RR"), "Okno danych - 5 minut"],
-                                     loc='upper left', numpoints=5)
-    plt.setp(leg.get_texts(), fontsize='large')
-    plt.setp(leg.get_texts(), fontweight='bold')
-
-    bold_ticks_labels(ax_right_tachogram)
-
-    arrow_size = 4
-    arrow_window_right = mpatches.Arrow(max_idx / 2 - arrow_size / 2,
-                                        des1_max - des1_max / 35,
-                                        4, 0,
-                                        width=100, color="red") #, zorder=3)
-    arrow_window_right.set_fill(True)
-    ax_right_tachogram.add_patch(arrow_window_right)
-    row_number = row_number + 1
-
-
-make_ticklabels_invisible(plt.gcf())
+arrow_size = 4
+arrow_window_right = mpatches.Arrow(max_idx / 2 - arrow_size / 2,
+                                    des1_max - des1_max / 35,
+                                    4, 0,
+                                    width=100, color="red") #, zorder=3)
+arrow_window_right.set_fill(True)
+ax_right_tachogram.add_patch(arrow_window_right)
 
 plt.show()
-
